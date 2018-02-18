53b3a4a574b9a7b9338f2130177a25a29359b5a2
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-02-18 11:18:15 -0500

    make the summary statistics more readable

--- explore.py
+++ explore.py
@@ -4,13 +4,17 @@ import datafiles
 dest = open("output/summaries.txt", "w+")
 for year in [2007,2017]:
   for filename in datafiles.files[year]:
-    dest.write("\n\n" + filename)
+    dest.write("\n\ndataset: " + filename)
     df = pd.read_csv( datafiles.folder(year) + "recip-100/"
                       + filename + ".csv")
     for colname in list(df.columns.values):
-      dest.write("\n\t" + colname)
+      dest.write("\n\n\t" + colname)
       col = df[colname]
       dest.write("\n\t" + "missing: " + str(len(col.index)-col.count()) )
-      if col.dtype == object: dest.write("\n\n\t" + str(col.unique()) + "\n")
+      if col.dtype == object:
         # bounding unique() list with "\n" so it is easily skipped if long
-      else: dest.write("\n\t" + str(col.describe()) )
+        dest.write("\n\n\t" + str(col.unique()) + "\n")
+      else:
+        description = map(lambda x: '\t' + x
+                          , str(col.describe()).split('\n') )
+        dest.write("\n" + "\n".join(description))
