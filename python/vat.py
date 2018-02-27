5f473eee48d2562c05b9a34d0ff2c6cf7e6eee0d
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-02-27 17:45:20 -0500

    begin computing taxes based on coicop

--- python/vat.py
+++ python/vat.py
@@ -2,18 +2,9 @@
 
 import numpy as np
 import pandas as pd
-import util as util
+import python.util as util
 
-taxes = pd.DataFrame.from_csv("data/toy/taxes.csv")
-spending = pd.DataFrame.from_csv("data/toy/spending.csv")
+taxRates = pd.DataFrame( { 'coicop' : range(1,1500)
+                   , 'taxRate' : 0.19 } )
+taxRates.loc[taxRates['coicop'] < 500, 'taxRate'] = 0.05
 
-taxes["tax"] = taxes["price"] * taxes["taxRate"] # do once, not per person
-
-if not (taxes.index == spending.index).all():
-    util.printInRed(
-        "Error: The indices (lists of goods) for tax rates and spending differ." )
-else:
-    whatWhoPaid = people.apply( lambda row:
-                                np.dot( row, taxes["tax"] ) )
-    whatWhoPaid.name = "totalVatPaid"
-    spending = spending.append(whatWhoPaid)
