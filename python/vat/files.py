185c1e1c2b0d35f874c96f233023cf8546d4f40a
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-03-01 08:40:42 -0500

    collect from all files

--- python/vat/files.py
+++ python/vat/files.py
@@ -2,17 +2,67 @@ legends = { # keys are filenames
             # values are dictionaries from column names to their meaning
   "st2_sea_enc_gcfhr_ce_csv" : { "nc2r_ce_p2"   : "coicop"
                                , "nc2r_ce_p4s1" : "quantity"
-                               , "nc2r_ce_p4s2" : "unit of measure"
+                               , "nc2r_ce_p4s2" : "unit-of-measure"
                                , "nc2r_ce_p5"   : "purchased=1"
                                , "nc2r_ce_p6"   : "where-bought"
                                , "nc2r_ce_p7"   : "value"
-                               , "nc2r_ce_p8"   : "frequency-bought" }
+                               , "nc2r_ce_p8"   : "frequency" }
   , "st2_sea_enc_gcfhr_csv" : { "nh_cgprcfh_p1"   : "good-in-words"
                               , "nh_cgprcfh_p1s1" : "coicop"
                               , "nh_cgprcfh_p2"   : "quantity"
                               , "nh_cgprcfh_p3"   : "purchased=1"
                               , "nh_cgprcfh_p4"   : "where-bought"
                               , "nh_cgprcfh_p5"   : "value"
-                              , "nh_cgprcfh_p6"   : "frequency-bought"
+                              , "nh_cgprcfh_p6"   : "frequency"
                               , "nh_cgprcfh_p7"   : "household-communal" }
+  , "st2_sea_enc_gcfhu_diarios_csv" : {
+      "nh_cgducfh_p1"   : "good-in-words"
+    , "nh_cgducfh_p1_1" : "coicop"
+    , "nh_cgducfh_p2"   : "quantity"
+    , "nh_cgducfh_p3"   : "purchased=1"
+    , "nh_cgducfh_p4"   : "where-bought"
+    , "nh_cgducfh_p5"   : "value"
+    , "nh_cgducfh_p6"   : "frequency"
+    , "nh_cgducfh_p7"   : "household-communal" }
+  , "st2_sea_enc_gcfhup_diarios_csv" : {
+      "nh_cgpucfh_p1"    : "good-in-words"
+    , "nh_cgpucfh_p1_s1" : "coicop"
+    , "nh_cgpucfh_p2"    : "quantity"
+    , "nh_cgpucfh_p3"    : "purchased=1"
+    , "nh_cgpucfh_p4"    : "where-bought"
+    , "nh_cgpucfh_p5"    : "value"
+    , "nh_cgpucfh_p6"    : "frequency" }
+  , "st2_sea_enc_gdr_csv" : {
+      "nc2r_ca_p3" : "coicop"
+    , "nc2r_ca_p4_s1" : "quantity"
+    , "nc2r_ca_p4_s2" : "unit-of-measure"
+    , "nc2r_ca_p5_s1" : "purchased=1"
+    , "nc2r_ca_p6_s1" : "where-bought"
+    , "nc2r_ca_p7_s1" : "value"
+    , "nc2r_ca_p8_s1" : "frequency" }
+  , "st2_sea_enc_gmf_csv" : {
+      "p10270" : "coicop"
+    , "p10270_fc_s1" : "purchased=1"
+    , "p10270s1" : "value(total/cash)"
+    , "p10270s2" : "where-bought"
+    , "p10270s3" : "frequency"
+    , "p10270s4" : "value(credit)"
+    }
+  , "st2_sea_enc_gsdp_diarios_csv" : {
+     "nc4_cc_p1_1" : "coicop"
+    , "nc4_cc_p2" : "quantity"
+    , "nc4_cc_p3" : "purchased=1"
+    , "nc4_cc_p4" : "where-bought"
+    , "nc4_cc_p5" : "value"
+    , "nc4_cc_p6" : "frequency" }
+  , "st2_sea_enc_gsdu_diarios_csv" : {
+      "nh_cgdu_p1" : "coicop"
+    , "nh_cgdu_p2" : "quantity"
+    , "nh_cgdu_p3" : "unit-of-measure"
+    , "nh_cgdu_p5" : "purchased=1"
+    , "nh_cgdu_p7b1379" : "where-bought"
+    , "nh_cgdu_p8" : "value"
+    , "nh_cgdu_p9" : "frequency"
+    , "nh_cgdu_p10" : "household-communal"
+    }
 }
