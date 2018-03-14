168d16985767c800f7ab64d7be1bb26d3b42617d
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-03-14 14:24:53 -0500

    add a function to augment descriptives with a count of missing values

--- python/util.py
+++ python/util.py
@@ -2,3 +2,11 @@ def printInRed(message):
     """from https://stackoverflow.com/a/287934/916142"""
     CSI="\x1B["
     print( CSI+"31;40m" + message + CSI + "0m")
+
+def describeWithMissing(df):
+  x = df.describe()
+  y = []
+  yy = pd.DataFrame( df.isnull().sum()
+                     , columns = ["missing"]
+                   ).transpose()
+  return yy.append( x )
