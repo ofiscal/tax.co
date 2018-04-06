cefd36ac7abab64f668638eb9606f37417904a07
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-04-06 16:23:55 -0500

    separate vat output io into a separate library make subsample an argument, not a global variable, in the *Stage functions

--- python/vat/build.py
+++ python/vat/build.py
@@ -4,27 +4,18 @@ import numpy as np
 import pandas as pd
 import python.util as util
 import python.datafiles as datafiles
-import python.vat.files as vatfiles
-import os
+import python.vat.files as vat_files
+import python.vat.output_io as vat_output_io
 
 
-subsample = 1 # Reciprocal of subsample size. Valid: 1, 10, 100, 1000.
-purchases = pd.DataFrame() # will accumulate from each file
-files = list( vatfiles.purchase_file_legends.keys() )
-
-def saveStage(data,name):
-  path = 'output/vat-data/recip-' + str(subsample)
-  if not os.path.exists(path): os.makedirs(path)
-  data.to_csv( path + '/' + name + ".csv" )
-
-def readStage(name): # to skip rebuilding something
-  path = 'output/vat-data/recip-' + str(subsample)
-  return pd.read_csv( path + '/' + name + ".csv" )
+subsample = 100 # Reciprocal of subsample size. Valid: 1, 10, 100, 1000.
+files = list( vat_files.purchase_file_legends.keys() )
 
 
 if True: # build the purchase data
+  purchases = pd.DataFrame() # will accumulate from each file
   for file in files:
-    legend = vatfiles.purchase_file_legends[file]
+    legend = vat_files.purchase_file_legends[file]
     shuttle = pd.read_csv(
       datafiles.yearSubsampleSurveyFolder(2017,subsample) + file + '.csv'
       , usecols = list( legend.keys() )
@@ -43,7 +34,7 @@ if True: # build the purchase data
 
     purchases = purchases.append(shuttle)
   del(shuttle)
-  saveStage(purchases, '/1.purchases')
+  vat_output_io.saveStage(subsample, purchases, '/1.purchases')
 
 
 if True: # merge coicop, build money-valued variables
@@ -53,29 +44,29 @@ if True: # merge coicop, build money-valued variables
   purchases["price"] = purchases["value"] / purchases["quantity"]
   purchases["1-value"] = purchases["value"]
   purchases["1-quantity"] = purchases["quantity"]
-  purchases["frequency"].replace( vatfiles.frequency_legend, inplace=True )
+  purchases["frequency"].replace( vat_files.frequency_legend, inplace=True )
   purchases["value"] = purchases["frequency"] * purchases["value"]
   purchases["vat-paid"] = purchases["value"] * purchases["vat-rate"]
 
-  saveStage(purchases, '/2.purchases,prices,taxes')
+  vat_output_io.saveStage(subsample, purchases, '/2.purchases,prices,taxes')
 
 
-# purchases = readStage('/2.purchases,prices,taxes')
+# purchases = vat_output_io.readStage(subsample,'/2.purchases,prices,taxes')
 if True: # build the person expenditure data
   purchases["transactions"] = 1
   people = purchases.groupby(
     ['household', 'household-member'])['value','vat-paid',"transactions"].agg('sum')
   people = people.reset_index(level = ['household', 'household-member'])
-  saveStage(people, '/3.person-level-expenditures')
+  vat_output_io.saveStage(subsample, people, '/3.person-level-expenditures')
   
 
-#people = readStage('/3.person-level-expenditures')
+#people = vat_output_io.readStage(subsample, '3.person-level-expenditures')
 if True: # merge demographic statistics
   # PITFALL: Even if using a subsample of purchases, use the complete demographic data sample
   demog = pd.read_csv( datafiles.yearSubsampleSurveyFolder(2017,1) + 'st2_sea_enc_per_csv.csv'
-                    , usecols = list( vatfiles.person_file_legend.keys() )
+                    , usecols = list( vat_files.person_file_legend.keys() )
   )
-  demog = demog.rename(columns=vatfiles.person_file_legend) # homogenize column names across files
+  demog = demog.rename(columns=vat_files.person_file_legend) # homogenize column names across files
 
   if True: # normalize some categorical variables to be 0-1
     demog["female"] =   demog["female"] - 1          # orig 1=male,2=female
@@ -87,7 +78,7 @@ if True: # merge demographic statistics
     demog["r-palenq"] =   demog["race"] == 4
     demog["r-neg|mul"] =  demog["race"] == 5
     demog["r-whi|mest"] = demog["race"] == 6
-  saveStage(demog, '/4.demog')
+  vat_output_io.saveStage(subsample, demog, '/4.demog')
 
   people = pd.merge( people, demog, how = "right"
                      , on=["household","household-member"] )
@@ -96,10 +87,10 @@ if True: # merge demographic statistics
   people["vat/inc"] = people["vat-paid"]/people["income"]
 
   del(demog)
-  saveStage(people, '/5.person-demog-expenditures')
+  vat_output_io.saveStage(subsample, people, '/5.person-demog-expenditures')
 
 
-# people = readStage('/5.person-demog-expenditures')
+# people = vat_output_io.readStage(subsample, '/5.person-demog-expenditures')
 if True: # aggregate from household-members to households
   people["members"] = 1
   h_sum = people.groupby(
@@ -139,6 +130,6 @@ if True: # aggregate from household-members to households
 
   households["household"] = households.index
     # when there are multiple indices, reset_index is the way to do that
-  saveStage(households, '/6.households')
+  vat_output_io.saveStage(subsample, households, '/6.households')
 
-# households = readStage('/6.households')
+# households = vat_output_io.readStage(subsample, '/6.households')
--- python/vat/load-commands.py
+++
@@ -1,9 +0,0 @@
-# Read into memory the data generated in build.py
-# These use python.vat.build.readStage
-
-purchasesEarly = readStage('/1.purchases') # memory hog
-purchases = readStage('/2.purchases,prices,taxes') # memory hog
-
-demog = readStage('/4.demog')
-people = readStage('/5.person-demog-expenditures')
-households = readStage('/6.households')
