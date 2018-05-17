d3985847ee7b2a7490761845035e31b7968d9bd2
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-05-16 20:38:55 -0500

    make cdf-drawing a function callable on arbitrarily-shaped dataets

--- python/vat/pdf-cdf.py
+++ python/vat/pdf-cdf.py
@@ -6,26 +6,33 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-## Some toy data
+the_data = pd.DataFrame([1,2,7,2,7],columns=["x"])
 
-data = pd.DataFrame([1,2,2,7,7],columns=["x"])
-data["count"] = 1
-nonzero_pdf = data.groupby("x").agg('sum')
-nonzero_pdf["nonzero_pdf"] = nonzero_pdf["count"] / nonzero_pdf["count"].sum()
-nonzero_pdf = nonzero_pdf.reset_index(level="x")
+def draw_cdf( series ):
+  data = pd.DataFrame()
+  data["x"] = pd.Series( sorted(series) )
+  data["count"] = 1
 
-pdf_range = pd.DataFrame()
-pdf_range["x"] = np.arange(0,10,0.01) # critical : the last number here must be small
+  dmin = data["x"].min()
+  dmax = data["x"].max()
+  dstep = (dmax - dmin) / 100 # this resolution is arbitrary
 
-df = pdf_range.merge( nonzero_pdf, on = "x", how = "outer" )
-df["pdf"] = np.where( df["nonzero_pdf"].isnull(), 0, df["nonzero_pdf"] )
-df = df[["x","pdf"]]
-df["cdf"] = df["pdf"].cumsum()
+  nonzero_pdf = data.groupby("x").agg('sum') # only the nonzero part of the pdf
+  nonzero_pdf["nonzero_pdf"] = nonzero_pdf["count"] / nonzero_pdf["count"].sum()
+  nonzero_pdf = nonzero_pdf.reset_index(level="x")
 
+  pdf_range = pd.DataFrame()
+  pdf_range["x"] = np.arange( dmin - 10*dstep, dmax + 10*dstep, dstep )
+    # this 10-step (5%) buffer is arbitrary
 
-## draw it
+  df = pdf_range.merge( nonzero_pdf, on = "x", how = "outer" )
+  df["pdf"] = np.where( df["nonzero_pdf"].isnull(), 0, df["nonzero_pdf"] ) # np.where is like if-else
+  df = df[["x","pdf"]]
+  df = df.sort_values("x")
+  df["cdf"] = df["pdf"].cumsum()
 
-plt.hist( df["x"],df["pdf"] )
-plt.plot( df["x"],df["cdf"], 'r--' )
-plt.show()
-plt.savefig("test.png")
+  # plt.plot( df["x"],df["pdf"] ) # could overlay.  in discrete distributions it's 0 a.e.
+  plt.plot( df["x"],df["cdf"], 'r--' )
+
+draw_cdf( the_data["x"] )
+plt.show()   # or plt.savefig("test.png")
