ecf227a646e0936bfc587dfe4c4db8ebd2babede
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-05-17 22:09:41 -0500

    draw/examples.py: make it executable without pre-importing other stuff

--- python/vat/draw/examples.py
+++ python/vat/draw/examples.py
@@ -1,5 +1,16 @@
+import python.vat.draw.util as draw
+import pandas as pd
+
+%matplotlib inline
+  # enable the previous line if calling from Jupyter
+import matplotlib
+# matplotlib.use('Agg')
+  # enable the previous line if calling from the (non-gui) shell
+import matplotlib.pyplot as plt
+
+
 the_data = pd.DataFrame([1,2,7,2,7],columns=["x"])
-draw_cdf( the_data["x"] )
+draw.cdf( the_data["x"] )
 if True: # alternatives
   # plt.show()
   plt.savefig("test.png")
--- python/vat/draw/util.py
+++ python/vat/draw/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-%matplotlib inline
+# %matplotlib inline
   # enable the previous line if calling from Jupyter
 import matplotlib
 matplotlib.use('Agg')
@@ -9,7 +9,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def draw_cdf( series ):
+def cdf( series ):
   data = pd.DataFrame()
   data["x"] = pd.Series( sorted(series) )
   data["count"] = 1
@@ -18,19 +18,22 @@ def draw_cdf( series ):
   dmax = data["x"].max()
   dstep = (dmax - dmin) / 100 # this resolution is arbitrary
 
-  nonzero_pdf = data.groupby("x").agg('sum') # only the nonzero part of the pdf
-  nonzero_pdf["nonzero_pdf"] = nonzero_pdf["count"] / nonzero_pdf["count"].sum()
-  nonzero_pdf = nonzero_pdf.reset_index(level="x")
+  pdf = data.groupby("x").agg('sum')
+    # only the nonzero part of the pdf
+  pdf["nonzero_pdf"] = pdf["count"] / pdf["count"].sum()
+  pdf = pdf.reset_index(level="x")
 
   pdf_range = pd.DataFrame()
   pdf_range["x"] = np.arange( dmin - 10*dstep, dmax + 10*dstep, dstep )
     # this 10-step (5%) buffer is arbitrary
 
-  df = pdf_range.merge( nonzero_pdf, on = "x", how = "outer" )
-  df["pdf"] = np.where( df["nonzero_pdf"].isnull(), 0, df["nonzero_pdf"] ) # np.where is like if-else
+  df = pdf_range.merge( pdf, on = "x", how = "outer" )
+  df["pdf"] = np.where( df["nonzero_pdf"].isnull(), 0, df["nonzero_pdf"] )
+    # np.where is like if-else
   df = df[["x","pdf"]]
   df = df.sort_values("x")
   df["cdf"] = df["pdf"].cumsum()
 
-  # plt.plot( df["x"],df["pdf"] ) # could overlay.  in discrete distributions it's 0 a.e.
+  # plt.plot( df["x"],df["pdf"] )
+    # could overlay, but in discrete distributions, is zero almost everywhere
   plt.plot( df["x"],df["cdf"], 'r--' )
