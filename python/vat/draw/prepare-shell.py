f3917ac0e494a7d826edf728c14890a5d5f101fc
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-05-18 19:12:18 -0500

    store some common library-loading boilerplate

---
+++ python/vat/draw/prepare-shell.py
@@ -0,0 +1,12 @@
+import python.vat.draw.util as draw
+import python.vat.output_io as oio
+
+import python.vat.draw.util as draw
+import pandas as pd
+
+# %matplotlib inline
+  # enable the previous line if calling from Jupyter
+import matplotlib
+matplotlib.use('Agg')
+  # enable the previous line if calling from the (non-gui) shell
+import matplotlib.pyplot as plt
