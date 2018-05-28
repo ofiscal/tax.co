add80b3011190b3722d8fda5b689487b94c39689
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-05-27 22:29:41 -0500

    more pictures

--- python/vat/report/pics.py
+++ python/vat/report/pics.py
@@ -1,8 +1,25 @@
 vat_pics_dir = "output/vat-pics/"
 
+
+if True: # summary stats about people
+  plt.close()
+  draw.single_cdf( people["value"], "Spending per month",
+                   vat_pics_dir + "people/" + "spending per month",
+                   logx = True)
+
+  plt.close()
+  draw.single_cdf( people["value"], "Income",
+                   vat_pics_dir + "people/" + "income",
+                   logx = True)
+
+  plt.close()
+  draw.single_cdf( people["transactions"], "Transactions per month",
+                   vat_pics_dir + "people/" + "transactions per month")
+
+
 if True: # summary stats about households
   plt.close()
-  draw.single_cdf( households["members"], "Household Size",
+  draw.single_cdf( households["members"], "Household size",
                    vat_pics_dir + "households/" + "size")
 
   plt.close()
@@ -23,19 +40,27 @@ if True: # summary stats about households
                    logx = True)
 
   plt.close()
+  plt.title("Highest education level among household members")
   plt.xticks( np.arange(1,10,1),
               [ "Ninguno",
                 "Preescolar",
-                "Basica Primaria",
-                "Basica Secundaria",
+                "Basica\n Primaria",
+                "Basica\n Secundaria",
                 "Media",
-                "Superior o Universitaria",
-                "(unused value)",
-                "(unused value)",
-                "No sabe, no informa" ],
+                "Superior o\n Universitaria",
+                "(unused\n value)",
+                "(unused\n value)",
+                "No sabe,\n no informa" ],
               rotation='vertical')
-  draw.single_cdf( households["edu-max"], "Highest education level among household members",
-                   vat_pics_dir + "households/" + "max edu")
+  plt.gcf().subplots_adjust(bottom=0.30) # labels go out of frame otherwise
+  df = pd.DataFrame(
+    households.groupby( "edu-max" )["edu-max"] \
+      .agg('sum') )                            \
+    .rename( columns = {"edu-max":"count"} )   \
+    .reset_index( level = "edu-max" )
+  plt.bar( df["edu-max"], df["count"] )
+  plt.savefig( vat_pics_dir + "households/" + "max edu" )
+
 
 if True: # the CDF of (VAT / consumption) by income decile
   # PITFALL: Because 47% of households report zero income, there is nothing
@@ -50,7 +75,8 @@ if True: # the CDF of (VAT / consumption) by income decile
   maxs = households.groupby( "income-decile" )[["income"]]    \
          .agg('max').rename(columns = {"income":"max"})
   decile_summary = pd.concat([counts,mins,maxs],axis=1)
-  
+
+  plt.close()  
   plt.title("The CDF of (VAT / consumption), by income decile")
   plt.xlabel("VAT paid / value consumed")
   plt.ylabel("Probability")
@@ -60,6 +86,7 @@ if True: # the CDF of (VAT / consumption) by income decile
     draw.cdf( households[ households["income-decile"]==i ]    \
                         ["vat/value"],
               linestyle = styles[i],
-              color = colors[i]
+              color = colors[i],
+              with_mean = False
     )
   plt.savefig(vat_pics_dir + "VAT over consumption by income decile.png")
