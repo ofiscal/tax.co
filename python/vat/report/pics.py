11894dabdb828f91a6a2b7bfa29cadd8c2420ef4
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-05-28 21:45:44 -0500

    describe (text) and draw series on individuals

--- python/vat/report/pics.py
+++ python/vat/report/pics.py
@@ -44,17 +44,25 @@ if True: # stats about purchases
 if True: # stats about people
   if True: # single series
     plt.close()
-    draw.single_cdf( people["value"], "Spending per month",
+    draw.single_cdf( people["age"], "CDF of age across individuals")
+    draw.savefig( vat_pics_dir + "people" , "age" )
+
+    plt.close()
+    draw.single_cdf( people["education"], "CDF of education across individuals")
+    draw.savefig( vat_pics_dir + "people" , "education" )
+
+    plt.close()
+    draw.single_cdf( people["value"], "CDF of spending per month across individuals",
                      logx = True)
     draw.savefig( vat_pics_dir + "people" , "spending per month" )
 
     plt.close()
-    draw.single_cdf( people["value"], "Income",
+    draw.single_cdf( people["value"], "CDF of income across individuals",
                      logx = True)
     draw.savefig( vat_pics_dir + "people" , "income" )
 
     plt.close()
-    draw.single_cdf( people["transactions"], "Transactions per month" )
+    draw.single_cdf( people["transactions"], "CDF of transactions per month across individuals" )
     draw.savefig( vat_pics_dir + "people" , "transactions per month" )
 
 
