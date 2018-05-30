afc743a16406e1aa007cfbc2bf261ab401d47add
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-05-30 15:16:34 -0500

    draw.cdf: modify how truncation works left-truncate household income CDF at 50,000   (in pesos, that's basically zero)

--- python/draw/util.py
+++ python/draw/util.py
@@ -42,6 +42,8 @@ def cdf( series, logx = False, with_mean = True, with_pdf = False, xmin = None, 
     plt.text( series.mean(), 0,
               "mean = " + format( series.mean(), '.2e') )
 
+  print(dmin,dmax)
+  plt.gca().set_xlim(left=dmin,right=dmax)
   plt.plot( df["x"],df["cdf"], **kwargs )
 
 
--- todo.md
+++ todo.md
@@ -1,1 +1,2 @@
+plot vat burden by education level
 household and individual income CDFs: plot on the same picture
