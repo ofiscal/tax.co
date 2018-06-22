2f4cfd8560defbf983159bcf4b9e726c692df09b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-06-22 17:28:34 -0500

    comments

--- python/vat/report/main.py
+++ python/vat/report/main.py
@@ -6,4 +6,7 @@ exec(open("python/draw/shell-load.py").read())
   # PITFALL: shell-load has to be called before anything else
   # that uses matplotlib, or it will automatically choose the wrong backend
 exec(open("python/vat/report/load.py").read())
+  # load some libraries and data sets specific to the VAT analysis
+  # (and build those data sets out a bit)
 exec(open("python/vat/report/pics.py").read())
+  # draw things
