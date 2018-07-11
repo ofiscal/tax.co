8a1379c4e8e2f54823401e633a24bc7c98545700
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-07-10 21:07:28 -0500

    bugfix   the bug: the categories and ordering on a series are lost     when it is stored in .csv format   the fix: redeclare them on import

--- python/vat/report/main,people.py
+++ python/vat/report/main,people.py
@@ -7,5 +7,18 @@ exec(open("python/vat/report/load.py").read())
 
 people = oio.readStage( subsample, '/5.person-demog-expenditures')
 
+# TODO: Centralize this code, which is (nearly) duplicated between build_late.py and main,people.py
+edu_key = { 1 : "Ninguno",
+      2 : "Preescolar",
+      3 : "Basica\n Primaria",
+      4 : "Basica\n Secundaria",
+      5 : "Media",
+      6 : "Superior o\n Universitaria",
+      9 : "No sabe,\n no informa" }
+people["education"] = pd.Series( pd.Categorical(
+    pd.Categorical( people["education"]
+                   , categories = list( edu_key.values() )
+                   , ordered = True) ) )
+
 exec(open("python/vat/report/pics,people.py").read())
   # draw things
