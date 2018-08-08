edc4d99abeb2d54c65ee35e19d8b3dcb2654d989
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-08 12:47:26 -0500

    purchases: duplicate all logx graphs in linear x

--- python/vat/report/pics,purchases.py
+++ python/vat/report/pics,purchases.py
@@ -1,7 +1,7 @@
-if True: # purchase quantity, with and without logs
+if True: # purchase quantity, logx and linear
   plt.close()
   draw.single_cdf( purchases["quantity"], "CDF of quantity per purchase",
-                  xmin = 1)
+                   xmin = 1, xmax = 1e3)
   draw.savefig( vat_pics_dir + "purchases" , "quantity" )
   
   plt.close()
@@ -26,12 +26,25 @@ plt.xticks( np.arange(1,11,1),
 draw.table( purchases, "frequency-code" )
 draw.savefig( vat_pics_dir + "purchases" , "frequency" )
 
-plt.close()
-draw.single_cdf( purchases["value"], "CDF of monthly purchase value",
-                 xmin =1, logx = True)
-draw.savefig( vat_pics_dir + "purchases" , "value" )
+if True: # purchase value, logx and linear
+  plt.close()
+  draw.single_cdf( purchases["value"], "CDF of monthly purchase value",
+                   xmin=1, xmax=1e5)
+  draw.savefig( vat_pics_dir + "purchases" , "value" )
 
-plt.close()
-draw.single_cdf( purchases["vat-paid"], "CDF of VAT paid per purchase",
-                 xmin = 1, logx = True)
-draw.savefig( vat_pics_dir + "purchases" , "vat-in-pesos" )
+  plt.close()
+  draw.single_cdf( purchases["value"], "CDF of monthly purchase value",
+                   xmin =1, logx = True)
+  draw.savefig( vat_pics_dir + "purchases/logx" , "value" )
+
+if True: # VAT per purchase, logx and linear  
+  plt.close()
+  draw.single_cdf( purchases["vat-paid"], "CDF of VAT paid per purchase",
+                   xmin = 1, xmax = 1e4)
+  draw.savefig( vat_pics_dir + "purchases" , "vat-in-pesos" )
+
+  plt.close()
+  draw.single_cdf( purchases["vat-paid"], "CDF of VAT paid per purchase",
+                   xmin = 0.01, logx = True)
+  draw.savefig( vat_pics_dir + "purchases/logx" , "vat-in-pesos" )
+  
