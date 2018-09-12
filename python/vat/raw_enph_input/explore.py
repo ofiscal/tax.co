9c282a0877da9bcef1530da4be2a5f100ce0df1a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-09-12 15:05:59 -0500

    catalog the how-got variable legends

--- python/vat/raw_enph_input/explore.py
+++ python/vat/raw_enph_input/explore.py
@@ -1,28 +1,28 @@
 # To check the values present in the data against those described in the data documentation.
 
-filenames = [
-    "urban_personal_fuera"
+file_names = [
+    "articulos"
+  , "medios"
+  , "rural capitulo c"
+  , "urban capitulo c"
+  , "rural_personal"
+  , "rural_personal_fuera"
+  , "rural_semanal"
+  , "rural_semanal_fuera"
+  , "urban_diario"
   , "urban_diario_fuera"
   , "urban_diario_personal"
-  , "urban_diario"
-  , "rural_personal_fuera"
-  , "rural_personal"
-  , "rural_semanal_fuera"
-  , "rural_semanal"
-  , "articulos"
-  , "medios"
-  , "urban capitulo c"
-  , "rural capitulo c"
+  , "urban_personal_fuera"
   ]
 
 df = enph.purchases
 
-def check(filename):
-  dff = df[ df["file-origin"]==filename ]
-  freqs = list( dff["freq"].unique() )
-  for i in sorted( [str(fr) for fr in freqs] ):
+def check(file_name,col_name):
+  dff = df[ df["file-origin"]==file_name ]
+  vals = list( dff[col_name].unique() )
+  for i in sorted( [str(v) for v in vals] ):
     print(i)
 
-for fn in filenames:
+for fn in file_names:
   print("\n\n" + fn)
-  check(fn)
+  check(fn,"how-got")
