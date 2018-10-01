afa9675c625e532d1c5b487393718934eb45f183
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-10-01 14:13:12 -0500

    rename columns; complete transfer to people.py

--- python/vat/build/people.py
+++ python/vat/build/people.py
@@ -16,40 +16,95 @@ files = [
       , "P6170"      : "student"
       , "P6210"      : "education" # highest level completed
 
-****** P8610S1 : income, yearly : edu, beca, cash
-****** P8610S2 : income, yearly : edu, beca, in-kind
-****** P8612S1 : income, yearly : edu, non-beca, cash
-****** P8612S2 : income, yearly : edu, non-beca, in-kind
+P8610S1 : income, year : grant : edu, beca
+P8610S2 : income, year : grant : edu, beca, in-kind
+P8612S1 : income, year : grant : edu, non-beca
+P8612S2 : income, year : grant : edu, non-beca, in-kind
 
-******* P6500 = income, monthly : formal employment
-        # was  "P6500"      : "labor income, formal" # the quantity referred to by the "overlooked" questions
-******* P6510S1 = income, monthly : overtime
-******* P6510S2 = income, monthly : overtime, included in 6500
-******* P6590S1 = income, monthly : food, in-kind
-******* P6600S1 = income, monthly : lodging, in-kind
-******* P6610S1 = income, monthly : transport, in-kind
-******* P6620S1 = income, monthly : other, in-kind
-******* P6585S1A1 = income subsidy, monthly : food
-******* P6585S1A2 = income subsidy, monthly : food, included in 6500
-******* P6585S2A1 = income subsidy, monthly : transport
-******* P6585S2A2 = income subsidy, monthly : transport, included in 6500
-******* P6585S3A1 = income subsidy, monthly : familiar
-******* P6585S3A2 = income subsidy, monthly : familiar, included in 6500
-******* P1653S1A1 = income, monthly : special bonus
-******* P1653S1A2 = income, monthly : special bonus, included in 6500
-******* P1653S2A1 = income, monthly : bonus
-******* P1653S2A2 = income, monthly : bonus, included in 6500
-******* P1653S3A1 = income, monthly : viaticum
-******* P1653S3A2 = income, monthly : viaticum, included in 6500
-******* P1653S4A1 = income, monthly : gastos de representacion
-******* P1653S4A2 = income, monthly : gastos de representacion, included in 6500
+P6500     = income, month : labor : formal employment
+P6510S1   = income, month : labor : overtime
+P6510S2   = income, month : labor : overtime, included in 6500
+P6590S1   = income, month : labor : food, in-kind
+P6600S1   = income, month : labor : lodging, in-kind
+P6610S1   = income, month : labor : transport, in-kind
+P6620S1   = income, month : labor : other, in-kind
+P6585S1A1 = income, month : labor : food
+P6585S1A2 = income, month : labor : food, included in 6500
+P6585S2A1 = income, month : labor : transport
+P6585S2A2 = income, month : labor : transport, included in 6500
+P6585S3A1 = income, month : labor : familiar
+P6585S3A2 = income, month : labor : familiar, included in 6500
+P1653S1A1 = income, month : labor : bonus ?2
+P1653S1A2 = income, month : labor : bonus ?2, included in 6500
+P1653S2A1 = income, month : labor : bonus
+P1653S2A2 = income, month : labor : bonus, included in 6500
+P1653S3A1 = income, month : labor : viaticum
+P1653S3A2 = income, month : labor : viaticum, included in 6500
+P1653S4A1 = income, month : labor : gastos de representacion
+P1653S4A2 = income, month : labor : gastos de representacion, included in 6500
 
-******* P6630S1A1 = income, yearly : prima de servicios
-******* P6630S2A1 = income, yearly : christmas bonus
-******* P6630S3A1 = income, yearly : vacation bonus
-******* P6630S4A1 = income, yearly : viaticum
-******* P6630S5A1 = income, yearly : bonus
-******* P6630S6A1 = income, yearly : work accident payments
+P6630S1A1 = income, year : labor : prima de servicios
+P6630S2A1 = income, year : labor : christmas bonus
+P6630S3A1 = income, year : labor : vacation bonus
+P6630S4A1 = income, year : labor : viaticum
+P6630S5A1 = income, year : labor : bonus
+P6630S6A1 = income, year : labor : work accident payments
+
+P6750 = income, month : labor : independent
+P676 = income, month : labor : independent, months
+  # divide P6750 by this to get monthly
+  # hopefully this is usually 1 or missing
+
+P550    : income, year  : labor : rural
+P6779S1 : income, month : labor : viaticum ?2
+
+P7070 = income, month : labor : job 2
+
+P9460S1 : income, month : benefit : unemployment
+P7422S1 : income, month : labor : as unemployed
+
+P7472S1 : income, month : labor : as inactive
+
+P7500S1A1 : income, month : rental : real estate, developed
+P7500S4A1 : income, month : rental : real estate, undeveloped
+P7500S5A1 : income, month : rental : vehicle | equipment
+
+P7500S2A1 : income, month : benefit : pension for age | illness
+P7510S1A1 = income, year : grant : remittance, domestic
+P7510S2A1 = income, year : grant : remittance, foreign
+P7510S3A1 = income, year : grant : from private domestic ?firms
+P7510S4A1 = income, year : grant : from private foreign ?firms
+
+P7510S5A1  = income, year : investment : interest
+P7510S6A1  = income, year : benefit : cesantia
+P7510S10A1 = income, year : investment : dividends
+P7510S9A1  = income, year : sale : ?stock
+
+P1668S1A1 : income, year : benefit : familias en accion
+P1668S1A4 : income, year : benefit : familias en accion, in-kind
+P1668S2A2 : income, year : benefit : programa de adultos mayores
+P1668S2A4 : income, year : benefit : programa de adultos mayores, in-kind
+P1668S3A2 : income, year : benefit : familias en su tierra
+P1668S3A4 : income, year : benefit : familias en su tierra, in-kind
+P1668S4A2 : income, year : benefit : jovenes en accion
+P1668S4A4 : income, year : benefit : jovenes en accion, in-kind
+P1668S5A2 : income, year : benefit : transferencias por victimizacion
+P1668S5A4 : income, year : benefit : transferencias por victimizacion, in-kind
+
+
+P7513S1A1 : income, year : sale : real estate
+P7513S2A1 : income, year : sale : vehicle | equipment
+P7513S3A1 : income, year : sale : livestock
+P7513S4A1 : income, year : sale : stock ?2
+P7513S5A1 : income, year : repayment : by person
+P7513S6A1 : income, year : repayment : by bank
+P7513S7A1 : income, year : repayment : by other
+
+P7513S8A1  : income, year : occasional : jury awards
+P7513S9A1  : income, year : occasional : gambling
+P7513S10A1 : income, year : occasional : inheritance
+P7513S11A1 : income, year : occasional : refund, tax
+P7513S12A1 : income, year : occasional : refund, other
 
       , "P7422S1"    : "labor income, ?1"
       , "P6500"      : "labor income, formal" # the quantity referred to by the "overlooked" questions
