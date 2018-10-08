1346d1bec6bc65d0872ea621bcc7fef79a14936c
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-10-08 15:45:09 -0500

    merge VAT rates into purchases

--- python/vat/build/main.py
+++ python/vat/build/main.py
@@ -17,7 +17,15 @@ if True: # buildings
 
 if True: # VAT dictionaries
   vat_cap_c = pd.read_csv( "data/vat/" + "vat-for-capitulo-c.csv"
-                         , encoding = "latin1" )
+                         , encoding = "latin1"
+              ) . rename( columns = { "CODE" : "25-broad-categs"
+                                    , "DESCRIPTION" : "description"
+              } )
   vat_coicop = pd.read_csv( "data/vat/" + "vat-by-coicop.csv"
                           , sep = ";"
                           , encoding = "latin1" )
+
+
+if True: # add VAT to purchases
+  purchases = purchases.merge( vat_coicop, how = "left", on="coicop" )
+  purchases = purchases.merge( vat_cap_c, how = "left", on="25-broad-categs" )
