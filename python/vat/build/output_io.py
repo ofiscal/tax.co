984d9a4acc3c29b144012e663d1d8486d25fbcb1
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-10-12 17:56:06 -0500

    bifurcate output_io.py for preliminary and official ENPH data

--- python/vat/build/output_io.py
+++ python/vat/build/output_io.py
@@ -3,12 +3,12 @@ import os
 
 
 def saveStage(subsample,data,name):
-  path = 'output/vat-prelim-data/recip-' + str(subsample)
+  path = 'output/vat-data/recip-' + str(subsample)
   if not os.path.exists(path): os.makedirs(path)
   data.to_csv( path + '/' + name + ".csv" )
 
 def readStage(subsample,name): # to skip rebuilding something
-  path = 'output/vat-prelim-data/recip-' + str(subsample)
+  path = 'output/vat-data/recip-' + str(subsample)
   return pd.read_csv( path + '/' + name + ".csv" )
 
 
