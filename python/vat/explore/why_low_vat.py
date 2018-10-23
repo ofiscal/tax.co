7c13a7118913de46dbb2aa2f83a07078827b69c9
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-10-23 14:57:50 -0500

    begin alternate vat rate matching strategies

---
+++ python/vat/explore/why_low_vat.py
@@ -0,0 +1,8 @@
+
+import python.vat.build.output_io as oio
+import python.util as util
+
+subsample = 10
+purchases = oio.readStage( subsample, "purchases_2_vat" )
+
+util.describeWithMissing( purchases[[ "25-broad-categs", "coicop" ]] )
