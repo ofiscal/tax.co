f532de896184db851cd0fba6f41d772a33e87db3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-10-30 18:29:42 -0500

    discard multilevel index

--- python/index-from-sort.py
+++ python/index-from-sort.py
@@ -13,4 +13,10 @@ def sort_household_by_income_then_make_index(df):
   dff["member-by-income"] = range(1, len(dff) + 1)
   return dff
 
-x = grouped.apply( sort_household_by_income_then_make_index )
+x = grouped . apply( sort_household_by_income_then_make_index
+          ) . drop( columns = "household"
+          ) . reset_index( # the top level of the index shares the name "household" and is equal to it
+          ) . drop( columns = "level_1" ) # the other part of the index is unneeded
+
+# By this point x is already a single data frame.
+
