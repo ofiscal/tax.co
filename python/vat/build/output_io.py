2ffac28c670dc6b66ac7723de983d8891e6a904b
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-11-06 13:25:37 -0500

    nothing (change var names)

--- python/vat/build/output_io.py
+++ python/vat/build/output_io.py
@@ -3,12 +3,13 @@ import os
 
 
 def saveStage(subsample,data,name,index=False):
-  path = 'output/vat/data/recip-' + str(subsample)
-  if not os.path.exists(path): os.makedirs(path)
-  data.to_csv( path + '/' + name + ".csv"
-             , index = index )
+  folder = 'output/vat/data/recip-' + str( subsample )
+  if not os.path.exists( folder ):
+    os.makedirs( folder )
+  path = folder + '/' + name + ".csv"
+  data.to_csv( path, index = index )
 
 def readStage(subsample,name,**kwargs):
-  path = 'output/vat/data/recip-' + str(subsample)
+  path = 'output/vat/data/recip-' + str( subsample )
   return pd.read_csv( path + '/' + name + ".csv"
                     , **kwargs )
