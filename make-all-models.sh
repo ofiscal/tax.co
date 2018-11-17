22c6981bac77b76c70c9d3cc66de757a105dc003
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-11-16 19:06:40 -0500

    add timestamps to make-all-models.sh

--- make-all-models.sh
+++ make-all-models.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-make overview subsample=$1 vat_strategy=approx vat_flat_rate=0.19
+echo; date; echo
+make overview subsample=$1 vat_strategy=detail
+echo; date; echo
+#make overview subsample=$1 vat_strategy=approx vat_flat_rate=0.19
+echo; date; echo
 make overview subsample=$1 vat_strategy=approx vat_flat_rate=0.18
 
-make overview subsample=$1 vat_strategy=detail
-
-make overview subsample=$1 vat_strategy=prop-2018-11-31 vat_flat_rate=0.18
-make overview subsample=$1 vat_strategy=prop-2018-11-31 vat_flat_rate=0.17
+echo; date; echo
+#make overview subsample=$1 vat_strategy=prop-2018-11-31 vat_flat_rate=0.18
+echo; date; echo
+#make overview subsample=$1 vat_strategy=prop-2018-11-31 vat_flat_rate=0.17
+echo; date; echo
 #make overview subsample=$1 vat_strategy=prop-2018-11-31 vat_flat_rate=0.16
+echo; date; echo
 #make overview subsample=$1 vat_strategy=prop-2018-11-31 vat_flat_rate=0.15
 
-make overview subsample=$1 vat_strategy=const vat_flat_rate=0.18
-make overview subsample=$1 vat_strategy=const vat_flat_rate=0.17
+echo; date; echo
+#make overview subsample=$1 vat_strategy=const vat_flat_rate=0.18
+echo; date; echo
+#make overview subsample=$1 vat_strategy=const vat_flat_rate=0.17
+echo; date; echo
 #make overview subsample=$1 vat_strategy=const vat_flat_rate=0.16
+echo; date; echo
 #make overview subsample=$1 vat_strategy=const vat_flat_rate=0.107
+echo; date; echo
