4c376c015467cd1e19422897e8f8e707d219288b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-11-20 11:38:47 -0500

    add the Ministry of Finance's bridge from coicop to vat rate org mode

--- make-all-models.sh
+++ make-all-models.sh
@@ -6,7 +6,7 @@ make overview subsample=$1 vat_strategy=detail
 
 # For each of the others we can specify some constant tax rates.
 for vat_strategy in approx; do
-    for vat_flat_rate in 0.19 0.18; do
+    for vat_flat_rate in 0.18; do
         echo; echo $vat_strategy $vat_flat_rate; date
         make overview subsample=$1 vat_strategy=$vat_strategy vat_flat_rate=$vat_flat_rate
     done
@@ -14,7 +14,7 @@ done
 
 # That list of tax rates can be empty, in which case the loop does nothing.
 for vat_strategy in prop-2018-11-31; do
-    for vat_flat_rate in 0.17; do
+    for vat_flat_rate in; do
         echo; echo $vat_strategy $vat_flat_rate; date
         make overview subsample=$1 vat_strategy=$vat_strategy vat_flat_rate=$vat_flat_rate
     done
