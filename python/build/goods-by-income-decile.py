9696b068d8a5f93a3e30a6d094e1eaf29b619af0
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-11-22 15:28:11 -0500

    goods by income decile: add verbal descriptions of goods

--- python/build/goods-by-income-decile.py
+++ python/build/goods-by-income-decile.py
@@ -13,9 +13,17 @@ ps = oio.readStage( common.subsample, 'purchases_1_5_no_origin'
                   , usecols = ["household", "value", "coicop"]  
 )
 
+rates = pd.read_csv( "data/vat/vat-by-coicop.csv"
+                   , sep=";"
+                   , usecols = ["description", "coicop"] )
+
+rates = rates.astype( {"coicop":"float"} )
+  # I would prefer to cast coicop to int in ps,
+  # but ints cannot hold NaN.
+
 ps = ps.merge( hs, on = "household" )
 
-out = ps  . groupby( ["income-decile","coicop"]
+grouped = ps  . groupby( ["income-decile","coicop"]
   ) . agg( {"value":"sum"}
   ) . sort_values( "value"
                  , ascending = False
@@ -26,5 +34,6 @@ out = ps  . groupby( ["income-decile","coicop"]
                  , ascending = [True,False]
 )
 
-out.to_csv( "output/vat/tables/recip-" + str(common.subsample) + "/goods_by_income_decile.csv" )
+out = grouped.merge( rates, how="left", on="coicop" )
 
+out.to_csv( "output/vat/tables/recip-" + str(common.subsample) + "/goods_by_income_decile.csv" )
