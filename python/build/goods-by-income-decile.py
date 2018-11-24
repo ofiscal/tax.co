335316946fd722580bfb4f2d2a224616d0cc591e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-11-23 19:43:20 -0500

    goods-by-income-decile: include tax rates   but don't exclude any purchases based on the associated tax rates

--- python/build/goods-by-income-decile.py
+++ python/build/goods-by-income-decile.py
@@ -17,7 +17,7 @@ hs = oio.readStage( common.subsample
 )
 
 ps = oio.readStage( common.subsample, 'purchases_1_5_no_origin'
-                  , usecols = ["household", "value", "coicop"]  
+                  , usecols = ["household", "value", "coicop"]
 )
 ps = ps[ ~ ps["coicop"] . isnull() ] # purchases coded by cap c, not coicop: discard
 ps["coicop"] = util.pad_column_as_int( 8, ps["coicop"] )
@@ -26,13 +26,17 @@ ps["coicop-3-digit"] = ps["coicop"] . apply( lambda s: s[0:3] )
 
 tax_proposed_2_digit = pd.read_csv( "python/build/vat_prop.2018_11_31/2-digit.csv"
                                     , usecols = ["coicop-2-digit","vat"] )
-tax_proposed_2_digit = tax_proposed_2_digit[ tax_proposed_2_digit["vat"] > 0 ]
+if False: # PITFALL: skipping, Luis wants to do by hand
+  tax_proposed_2_digit = tax_proposed_2_digit[ tax_proposed_2_digit["vat"] > 0 ]
+
 tax_proposed_2_digit["coicop-2-digit"] = (
   util.pad_column_as_int( 2, tax_proposed_2_digit["coicop-2-digit"] ) )
 
 tax_proposed_3_digit = pd.read_csv( "python/build/vat_prop.2018_11_31/3-digit.csv"
                                     , usecols = ["coicop-3-digit","vat"] )
-tax_proposed_3_digit = tax_proposed_3_digit[ tax_proposed_3_digit["vat"] > 0 ]
+if False: # PITFALL: skipping, Luis wants to do by hand
+  tax_proposed_3_digit = tax_proposed_3_digit[ tax_proposed_3_digit["vat"] > 0 ]
+
 tax_proposed_3_digit["coicop-3-digit"] = (
   util.pad_column_as_int( 3, tax_proposed_3_digit["coicop-3-digit"] ) )
 
@@ -48,24 +52,27 @@ if False: # PITFALL: skipping, Luis wants to do by hand
 ## ## ## ## ## Isolate to the "vat-marginal" COICOP codes ## ## ## ## ##
 # (that is, codes for which the current regime does not impose VAT while the proposal does)
 
-if False: # PITFALL: skipping; Luis wants to do by hand
-  ps = ps.merge( currently_untaxed, how = "right", on = "coicop" )
-  ps_2_digit = ps.merge( tax_proposed_2_digit, how = "right"
-                       , on="coicop-2-digit" )
-  ps_3_digit = ps.merge( tax_proposed_3_digit, how = "right"
-                       , on="coicop-3-digit" )
-  ps = ps_2_digit . combine_first( ps_3_digit
-       )
-  ps = ps[["household", "coicop", "value", "description"]]
+ps_vat = ps.merge( currently_untaxed, how = "inner", on = "coicop" )
+ps_2_digit = ps_vat.merge( tax_proposed_2_digit, how = "right"
+                     , on="coicop-2-digit" )
+ps_3_digit = ps_vat.merge( tax_proposed_3_digit, how = "right"
+                     , on="coicop-3-digit" )
+ps_vat = ps_2_digit . combine_first( ps_3_digit )
+ps_vat = ps_vat.rename( columns = { "vat, min" : "vat, current min"
+                                  , "vat" : "vat, proposed"
+} )
+ps_vat["vat, proposed"] = ps_vat["vat, proposed"] * 0.18
 
 
 ## ## ## ## ## Group by deciles ## ## ## ## ##
 
-ps = ps.merge( hs, on = "household" )
+ps_all = ps_vat.merge( hs, on = "household" )
 
-grouped = ps . groupby( ["income-decile","coicop"]
+grouped = ps_all . groupby( ["income-decile","coicop"]
   ) . agg( { "value":"sum"
-       # , "description": "first" # PITFALL: disabled while Luis does by hand
+           , "vat, current min" : "first"
+           , "vat, proposed" : "first"
+           , "description" : "first"
   } ) . sort_values( "value"
                  , ascending = False
   ) . reset_index(
@@ -75,16 +82,12 @@ grouped = ps . groupby( ["income-decile","coicop"]
                  , ascending = [True,False]
 )
 
-grouped = grouped.merge( currently_untaxed[["coicop","description"]]
-             , how = "left"
-             , on = "coicop" )
-
 grouped.to_csv( "output/vat/tables/recip-" + str(common.subsample) + "/goods_by_income_decile.csv" )
 
 
 ## ## ## ## ## The bottom 60% ## ## ## ## ##
 
-ps_60 = ps[ ps["income-decile"] <= 5]
+ps_60 = ps_all[ ps_all["income-decile"] <= 5]
 
 ps_60_grouped = ps_60 . groupby( "coicop"
   ) . agg( { "value":"sum"
@@ -108,7 +111,10 @@ grouped.to_csv( "output/vat/tables/recip-" + str(common.subsample) + "/goods,fir
 # A test
 
 if False: # TODO: HUnit
-  ( ps[ (ps["income-decile"] <= 5)
-        & (ps["coicop"] == ps_60_grouped.iloc[0]["coicop"])
-      ]["value"].sum()
-  ) == ps_60_grouped.iloc[0]["value"]
+  for i in range(0, len(ps_60_grouped)):
+    x = ( ps_all[ (ps_all["income-decile"] <= 5)
+                      & (ps_all["coicop"] == ps_60_grouped.iloc[i]["coicop"])
+                    ]["value"].sum()
+            ) == ps_60_grouped.iloc[i]["value"]
+    if not x: print( "TEST FAILED in goods-by-income-decile.py for i = " + str(i))
+  del(x)
