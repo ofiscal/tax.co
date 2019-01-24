1518c1ab3cdd380fef323a11c785d5d5d6bc5b92
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-01-24 17:37:52 -0500

    add functions for computing pensions   for asalariados and independientes   including employer contributions if applicable simplify an import

--- python/build/people_4_ss.py
+++ python/build/people_4_ss.py
@@ -1,8 +1,10 @@
 import sys
-import pandas as pd
+import pandas                    as pd
+
 import python.build.ss_schedules as ss
-import python.build.output_io as oio
-import python.build.common as c
+import python.build.output_io    as oio
+import python.util               as util
+import python.build.common       as c
 
 
 people = oio.readStage( c.subsample
@@ -10,8 +12,22 @@ people = oio.readStage( c.subsample
 
 people["4 por mil"] = 0.004 * (people["income, cash"] - 11.6e6)
 
-#def mk_pension ( independiente, income ):
-#  if independiente:
+def mk_pension( independiente, income ):
+  if independiente:
+    (_, compute_base, rate) = util.tuple_by_threshold(
+        income, ss.ss_contrib_schedule_for_contractor["pension"] )
+    return compute_base( income ) * rate
+  else:
+    (_, compute_base, rate) = util.tuple_by_threshold(
+        income, ss.ss_contrib_schedule_for_employee["pension"] )
+    return compute_base( income ) * rate
+
+def mk_pension_from_employer( independiente, income ):
+  if independiente: return 0
+  else:
+    (_, compute_base, rate) = util.tuple_by_threshold(
+        income, ss.ss_contribs_by_employer["pension"] )
+    return compute_base( income ) * rate
 
 # people = people.merge( << ss stuff >> )
 
--- python/build/ss_schedules.py
+++ python/build/ss_schedules.py
@@ -1,7 +1,6 @@
 # see rentas_naturales.xlsx for a model
 
-import python.build.common as c
-
+from python.build.common import min_wage
 
 # How to interpret these triples:
     # First number: threshold at which the regime starts
@@ -12,86 +11,86 @@ import python.build.common as c
 ss_contrib_schedule_for_contractor = {
   "pension" :
     [ ( 0, lambda labor_income: 0, 0.0 )
-    , ( c.min_wage
-      , lambda labor_income: min( max( 0.4*labor_income, c.min_wage )
-                                , 25*c.min_wage)
+    , ( min_wage
+      , lambda labor_income: min( max( 0.4*labor_income, min_wage )
+                                , 25*min_wage)
       , 0.16 ) ]
   , "salud" :
     [ ( 0
-      , lambda labor_income: min( max( 0.4*labor_income, c.min_wage )
-                                , 25*c.min_wage )
+      , lambda labor_income: min( max( 0.4*labor_income, min_wage )
+                                , 25*min_wage )
       , 0.125 ) ]
   , "solidaridad" :
     [ (0, lambda labor_income: 0, 0.0)
-    , ( 4*c.min_wage
-      , lambda labor_income: min( max(0.4*labor_income,c.min_wage)
-                                , 25*c.min_wage)
+    , ( 4*min_wage
+      , lambda labor_income: min( max(0.4*labor_income,min_wage)
+                                , 25*min_wage)
       , 0.01)
-    , ( 16*c.min_wage
-      , lambda labor_income: min( max(0.4*labor_income,c.min_wage)
-                                , 25*c.min_wage)
+    , ( 16*min_wage
+      , lambda labor_income: min( max(0.4*labor_income,min_wage)
+                                , 25*min_wage)
       , 0.012)
-    , ( 17*c.min_wage
-      , lambda labor_income: min( max(0.4*labor_income,c.min_wage)
-                                , 25*c.min_wage)
+    , ( 17*min_wage
+      , lambda labor_income: min( max(0.4*labor_income,min_wage)
+                                , 25*min_wage)
       , 0.014)
-    , ( 18*c.min_wage
-      , lambda labor_income: min( max(0.4*labor_income,c.min_wage)
-                                , 25*c.min_wage)
+    , ( 18*min_wage
+      , lambda labor_income: min( max(0.4*labor_income,min_wage)
+                                , 25*min_wage)
       , 0.016)
-    , ( 19*c.min_wage
-      , lambda labor_income: min( max(0.4*labor_income,c.min_wage)
-                                , 25*c.min_wage)
+    , ( 19*min_wage
+      , lambda labor_income: min( max(0.4*labor_income,min_wage)
+                                , 25*min_wage)
       , 0.018)
-    , ( 20*c.min_wage
-      , lambda labor_income: min( max(0.4*labor_income,c.min_wage)
-                                , 25*c.min_wage)
+    , ( 20*min_wage
+      , lambda labor_income: min( max(0.4*labor_income,min_wage)
+                                , 25*min_wage)
       , 0.02) ]
   }
 
 ss_contrib_schedule_for_employee = {
   "pension" :
-    [ ( 0,             lambda labor_income: 0                                      , 0.0)
-    , ( c.min_wage,    lambda labor_income: labor_income                           , 0.04)
-    , ( 13*c.min_wage, lambda labor_income: min( 0.7*labor_income, 25*c.min_wage)  , 0.04 ) ]
+    [ ( 0,           lambda labor_income: 0                                    , 0.0)
+    , ( min_wage,    lambda labor_income: labor_income                         , 0.04)
+    , ( 13*min_wage, lambda labor_income: min( 0.7*labor_income, 25*min_wage)  , 0.04 ) ]
   , "salud" :
-    [ ( 0,             lambda labor_income: 0                                      , 0.0 )
-    , ( c.min_wage,    lambda labor_income: labor_income                           , 0.04 )
-    , ( 13*c.min_wage, lambda labor_income: min( 0.7*labor_income, 25*c.min_wage ) , 0.04 ) ]
+    [ ( 0,           lambda labor_income: 0                                    , 0.0 )
+    , ( min_wage,    lambda labor_income: labor_income                         , 0.04 )
+    , ( 13*min_wage, lambda labor_income: min( 0.7*labor_income, 25*min_wage ) , 0.04 ) ]
   , "solidaridad" :
-    [ (0,              lambda labor_income: 0                                      , 0.0)
-    , (4*c.min_wage,   lambda labor_income:     labor_income                       , 0.01)
-    , (13*c.min_wage,  lambda labor_income: 0.7*labor_income                       , 0.01)
-    , (16*c.min_wage,  lambda labor_income: 0.7*labor_income                       , 0.012)
-    , (17*c.min_wage,  lambda labor_income: 0.7*labor_income                       , 0.014)
-    , (18*c.min_wage,  lambda labor_income: 0.7*labor_income                       , 0.016)
-    , (19*c.min_wage,  lambda labor_income: 0.7*labor_income                       , 0.018)
-    , (20*c.min_wage,  lambda labor_income: min(0.7*labor_income, 25*c.min_wage)   , 0.02) ]
+    [ (0,            lambda labor_income: 0                                    , 0.0)
+    , (4*min_wage,   lambda labor_income:     labor_income                     , 0.01)
+    , (13*min_wage,  lambda labor_income: 0.7*labor_income                     , 0.01)
+    , (16*min_wage,  lambda labor_income: 0.7*labor_income                     , 0.012)
+    , (17*min_wage,  lambda labor_income: 0.7*labor_income                     , 0.014)
+    , (18*min_wage,  lambda labor_income: 0.7*labor_income                     , 0.016)
+    , (19*min_wage,  lambda labor_income: 0.7*labor_income                     , 0.018)
+    , (20*min_wage,  lambda labor_income: min(0.7*labor_income, 25*min_wage)   , 0.02) ]
   }
 
-ss_contribs_by_employers = {
+ss_contribs_by_employer = {
   # For employees, but not contractors, some contributions are also made by the employer.
   "pension" :
-    [ ( 0,             lambda labor_income: 0                                    , 0.0)
-    , ( c.min_wage,    lambda labor_income: labor_income                         , 0.12)
-    , ( 13*c.min_wage, lambda labor_income: min(0.7*labor_income, 25*c.min_wage) , 0.12) ]
+    [ ( 0,           lambda labor_income: 0                                  , 0.0)
+    , ( min_wage,    lambda labor_income: labor_income                       , 0.12)
+    , ( 13*min_wage, lambda labor_income: min(0.7*labor_income, 25*min_wage) , 0.12) ]
   , "salud" :
-    [ ( 0,             lambda labor_income: 0                                    , 0.0)
-    , ( 10*c.min_wage, lambda labor_income: labor_income                         , 0.085)
-    , ( 13*c.min_wage, lambda labor_income: min(0.7*labor_income, 25*c.min_wage) , 0.085) ]
+    [ ( 0,           lambda labor_income: 0                                  , 0.0)
+    , ( 10*min_wage, lambda labor_income: labor_income                       , 0.085)
+    , ( 13*min_wage, lambda labor_income: min(0.7*labor_income, 25*min_wage) , 0.085) ]
   , "parafiscales" :
-    [ ( 0,             lambda labor_income: 0                                    , 0.0 )
-    , ( 10*c.min_wage, lambda labor_income: labor_income                         , 0.05 )
-    , ( 13*c.min_wage, lambda labor_income: min(0.7*labor_income, 25*c.min_wage) , 0.05 ) ]
+    [ ( 0,           lambda labor_income: 0                                  , 0.0 )
+    , ( 10*min_wage, lambda labor_income: labor_income                       , 0.05 )
+    , ( 13*min_wage, lambda labor_income: min(0.7*labor_income, 25*min_wage) , 0.05 ) ]
   , "cajas de compensación" :
-    [ ( 0,             lambda labor_income: 0                                    , 0.0)
-    , ( c.min_wage,    lambda labor_income: labor_income                         , 0.04)
-    , ( 13*c.min_wage, lambda labor_income: min(0.7*labor_income, 25*c.min_wage) , 0.04) ]
+    [ ( 0,           lambda labor_income: 0                                  , 0.0)
+    , ( min_wage,    lambda labor_income: labor_income                       , 0.04)
+    , ( 13*min_wage, lambda labor_income: min(0.7*labor_income, 25*min_wage) , 0.04) ]
   , "cesantías":
-    [ ( 0,             lambda labor_income: 0                                    , 0.0)
-    , ( c.min_wage,    lambda labor_income: labor_income, 2.12 / 12 )
+    [ ( 0,           lambda labor_income: 0                                  , 0.0)
+    , ( min_wage,    lambda labor_income: labor_income, 2.12 / 12 )
       # Every year a worker gets 1 prima de servicio worth 1 month's wages,
       # and 1 cesantia worth 1.12 month's wages. This formula gives the portion of
       # yearly income contributed each month to those.
-    , ( 13*c.min_wage, lambda labor_income: 0                                    , 0.0 ) ]
+    , ( 13*min_wage, lambda labor_income: 0                                  , 0.0 ) ]
   }
