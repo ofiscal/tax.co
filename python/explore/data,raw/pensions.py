74a96fb94ad35c0e1641b09f1fb80d268b43b06c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-02-11 18:29:25 -0500

    pensions: ungrouped stats: done

--- python/explore/data,raw/pensions.py
+++ python/explore/data,raw/pensions.py
@@ -36,41 +36,38 @@ ppl["pension, receiving"] = (   ( ppl["pension, contributing, pre"] == 3 )
                               | ( ppl["pension, receipts"] > 0 )
                             ) . astype('int')
 
-ppl["pension, contribution amount if >0"] = (
-  ppl["pension, contribution amount"]
-  .apply( lambda x: x if x>0 else np.nan) )
-
 ppl["pension, contributor(s) = split"] = (
-  ppl["pension, contributors, pre"] == 1
-  ) . astype( 'int' )
+  ppl["pension, contributors, pre"]
+  ) . apply( lambda x: 1 if x == 1 else
+             ( 0 if (x > 0) & (x < 4) else np.nan ) )
 
 ppl["pension, contributor(s) = self"] = (
-  ppl["pension, contributors, pre"] == 2
-  ) . astype( 'int' )
+  ppl["pension, contributors, pre"]
+  ) . apply( lambda x: 1 if x == 2 else
+             ( 0 if (x > 0) & (x < 4) else np.nan ) )
 
 ppl["pension, contributor(s) = employer"] = (
-  ppl["pension, contributors, pre"] == 3
-  ) . astype( 'int' )
+  ppl["pension, contributors, pre"]
+  ) . apply( lambda x: 1 if x == 3 else
+             ( 0 if (x > 0) & (x < 4) else np.nan ) )
 
 ppl["seguro de riesgos laborales"] = (
   ppl["seguro de riesgos laborales, pre"]
   . apply( lambda x: 1 if x==1 else (0 if x==2 else np.nan) ) )
+# ppl[["seguro de riesgos laborales", "seguro de riesgos laborales, pre"]]
 
 ppl["one"] = 1
-
-
 stats = []
 for col in ppl.columns:
   df = util.tabulate_stats_by_group( ppl, "one", col, "weight" )
   df["column"] = col
   stats.append(df)
 
-stats = pd.concat( stats ).reset_index().drop(columns="one")
-stats = stats[ stats["column"] != "one" ]
-
-# move the column named "column" to the front
-cols = list( stats.columns )
-cols.insert(0, cols.pop(cols.index("column")))
-stats = stats.ix[:, cols]
+stats = pd.concat( stats
+      ). set_index( "column"
+      ) . drop( index = ["household", "household-member", "weight"
+                        , "pension, contributing, pre"
+                        , "pension, contributors, pre"
+                        , "seguro de riesgos laborales, pre" ] )
 
 stats.to_csv( "output/pensions.csv" )
