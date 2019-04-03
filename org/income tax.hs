65de3899a7ac748bf3ec21fece99e5f5f3bfd5e2
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-04-03 17:49:59 -0500

    org

--- org/income tax.hs
+++ org/income tax.hs
@@ -1,62 +1,62 @@
 A dependent counts for 10% of the applicable renta liquida.
 
--- Common
-ing. no constitutivo de renta =
-  SS contributions
-  + GMF / 2
+-- common
+ingresos no constitutivo de renta =
+  ss contributions
+  + gmf / 2
   + things we don't have information about
 
--- Labor + pension income
-Renta liquida laboral = ingreso laboral
-  - ing laboral no constitutivo de renta
-Renta gravable laboral = renta liquida laboral
+-- labor + pension income
+renta liquida laboral = ingreso laboral
+  - ingresos laborales no constitutivos de renta
+renta gravable laboral = renta liquida laboral
   - f beneficios
   where f x = min( 40% renta gravable laboral
                  , 5040 uvt)
 
-Renta gravable pension = ingreso pension
+renta gravable pension = ingreso pension
   - ingreso pension no constitutivo de renta
   - renta exenta hasta mil uvt
 
-Impuesto tarifa uno aplica a:
-  renta gravable (pension + laboral) 
+impuesto tarifa uno aplica a:
+  renta gravable (pension + laboral)
 
--- Capital + nonlabor income
-Renta liquida capital = ingreso capital
+-- capital + nonlabor income
+renta liquida capital = ingreso capital
   - ingreso capital no constitutivo de renta
   where ingreso capital = interest
           + arriendos (literal rent, not profit)
           + regalias
-        ing K no constitutivo de renta = unknowable
+        ing k no constitutivo de renta = unknowable
 
-Renta gravable de capital = renta liquida capital
+renta gravable de capital = renta liquida capital
                             - f beneficios
   where f x = min ( 10 pct of renta gravable de capital
                   , 1000 uvt )
 
-Renta liquida no laboral = ingreso no laboral
+renta liquida no laboral = ingreso no laboral
   - ingreso no laboral no constiotutivo de renta
   where ingreso no laboral = short-term sales
         ingreso no laboral no constiotutivo de renta = unknowable
 
-Renta gravable no laboral = renta liquida no laboral
+renta gravable no laboral = renta liquida no laboral
   - f beneficios
   where f x = min ( 10 pct of renta gravable de capital
                   , 1000 uvt )
 
-Tarifa 2 applies to: 
-  Renta gravable (capital + no laboral)
+tarifa 2 applies to:
+  renta gravable (capital + no laboral)
 
--- Dividends: done
+-- dividends: done
 
--- Ingresos ocasionales
+-- ingresos ocasionales
 
-Renta gravable ocasional, 10%-taxable =
+renta gravable ocasional, 10%-taxable =
   long-term asset sales
   + inheritance
   + f (donations from private firms)
   where f x = x - min (20% x, 2290 uvt)
 
-Renta gravable ocasional, 20%-taxable =
+renta gravable ocasional, 20%-taxable =
   gambling
   + jury awards
