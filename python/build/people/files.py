0fb202d10610cef59331668dfdd48da9f837dca4
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-06-28 15:38:53 -0500

    factor out some common logic

--- python/build/people/files.py
+++ python/build/people/files.py
@@ -1,3 +1,4 @@
+from typing import Set
 import numpy as np
 import pandas as pd
 import python.build.classes as classes
@@ -216,26 +217,31 @@ files = [
       + beca_sources_govt
       + beca_sources_private
       + [ ("P6236", 0, "non-beca sources", 0) ]
-          # PITFALL: a space-separated list of ints
+          # PITFALL: Not a number. Instead, a space-separated list of ints.
     , c.corrections
 ) ]
 
-def count_public(list_as_str):
-  """Count public sources of funding in the "non-beca sources" variable."""
+def count_num_matches_in_space_separated_list(
+      list_as_str : str # a space-separated list of integers
+    , targets : Set[int]  ):
+  """ Counts how many times anything from the list argument appears in the string. """
   stripped = list_as_str . strip()
   if stripped in [np.nan, ""]: return 0
   else:
     num_list = map( float
                   , stripped . split(" ") )
-    sources = filter( lambda x: x in [2,3,4], num_list )
+    sources = filter( lambda x: x in targets, num_list )
     return len( list( sources ) )
 
-# count private sources of funding in the "non-beca sources" variable
-def count_private(list_as_str):
-  stripped = list_as_str . strip()
-  if stripped in [np.nan, ""]: return 0
-  else:
-    num_list = map( float
-                  , stripped . split(" ") )
-    sources = filter( lambda x: not x in [2,3,4], num_list )
-    return len( list( sources ) )
+public_codes = {2,3,4}
+private_codes = set( range(1,11) ) - public_codes
+
+def count_public( list_as_str : str ):
+  """ Count public sources of funding in the "non-beca sources" variable. """
+  return count_num_matches_in_space_separated_list(
+    list_as_str, public_codes )
+
+def count_private( list_as_str : str ):
+  """ Count private sources of funding in the "non-beca sources" variable. """
+  return count_num_matches_in_space_separated_list(
+    list_as_str, private_codes )
