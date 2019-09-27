61a99f1160b9e617f7e59cf6dcf85d55ac8c09e5
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-27 13:48:59 -0500

    vat holiday: create bulk of report

--- python/report/holiday.py
+++ python/report/holiday.py
@@ -9,33 +9,57 @@ if True: # bearings
   overviews_dir = "output/vat/tables/recip-" + str(c.subsample)
   stats = ["vat paid, min: mean",
            "vat paid, max: mean"]
-  who = [ "one: 1",
-          "income-decile: 0",
-          "income-decile: 1",
-          "income-decile: 2",
-          "income-decile: 3",
-          "income-decile: 4",
-          "income-decile: 5",
-          "income-decile: 6",
-          "income-decile: 7",
-          "income-decile: 8",
-          "income-percentile-in[90,97]: True",
-          "income-percentile: 98",
-          "income-percentile: 99" ]
+  who = {
+    "one: 1"                            : "all",
+    "income-decile: 0"                  : "income %ile 0-9",
+    "income-decile: 1"                  : "income %ile 10-19",
+    "income-decile: 2"                  : "income %ile 20-29",
+    "income-decile: 3"                  : "income %ile 30-39",
+    "income-decile: 4"                  : "income %ile 40-49",
+    "income-decile: 5"                  : "income %ile 50-59",
+    "income-decile: 6"                  : "income %ile 60-69",
+    "income-decile: 7"                  : "income %ile 70-79",
+    "income-decile: 8"                  : "income %ile 80-89",
+    "income-percentile-in[90,97]: True" : "income %ile 90-97",
+    "income-percentile: 98"             : "income %ile 98",
+    "income-percentile: 99"             : "income %ile 99" }
 
 if True:
+  dfs = {}
   def select_from( file : str ) -> pd.DataFrame:
     return ( pd.read_csv( overviews_dir + file,
                           index_col = "Unnamed: 0" )
-             [who] .
+             [list(who.keys())] .
+             rename(columns=who) .
              loc[stats] .
              transpose() )
   normal = select_from( "/overview.detail.2018.csv" )
   holiday = select_from( "/overview.vat_holiday_3.2018.csv" )
-  dfs = [normal,holiday]
+  dfs["normal"] = normal
+  dfs["holiday"] = holiday
 
-for df in dfs:
-  df["vat paid: mean"] = (
-    0.5 * ( df["vat paid, min: mean"] +
-            df["vat paid, max: mean"] ) )
+for k in ["normal","holiday"]:
+  dfs[k]["vat paid: mean"] = (
+    0.5 * ( dfs[k]["vat paid, min: mean"] +
+            dfs[k]["vat paid, max: mean"] ) )
+  dfs[k] = dfs[k].drop( columns = [ "vat paid, min: mean",
+                                    "vat paid, max: mean" ] )
 
+shift_window = 60
+  # If people can shift their purchase times by a month,
+  # then effectively the VAT holiday is two months,
+  # regardless of how long it is by law.
+report = (
+  pd.concat(
+    [ ( dfs["normal"] .
+        rename( columns = {"vat paid: mean":"normal"} ) ),
+      ( dfs["holiday"] .
+        rename( columns = {"vat paid: mean":"holiday"} ) ) ],
+    axis = "columns" ) )
+
+report["mix"] = (1/365) * (
+  (365 - shift_window) * dfs["normal"] +
+         shift_window  * dfs["holiday"]  )
+report["saved"]    = report["normal"] - report["mix"]
+report["saved, %"] = report["mix"] / report["normal"]
+report
