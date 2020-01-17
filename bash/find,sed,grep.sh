b234d8e2f61467178daf51ffec01babbba3bd967
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-01-17 17:05:08 -0500

    documentation   for all of bash/

--- bash/find,sed,grep.sh
+++ bash/find,sed,grep.sh
@@ -1,22 +1,29 @@
-# To avoid touching unmodified files:
-for i in *;do grep mytext $i && sed -i -e 's/mytext/replacement/g' $i;done
-  # https://stackoverflow.com/questions/27071019/sed-i-touching-files-that-it-doesnt-change
+######### What this is #########
+# This is NOT a script, but rather a collection of handy one-liners,
+# for various common tasks like renaming variables.
 
+# Find and replace without touching unmodified files.
+# (Otherwise `make` acts as if everything needs to be rerun.)
+# https://stackoverflow.com/questions/27071019/sed-i-touching-files-that-it-doesnt-change
+for i in *; do grep mytext $i && sed -i -e 's/mytext/replacement/g' $i; done
+
+# Find and replace in all files.
 find . -name "*.py" -print0 | xargs -0 sed -i "s/prop.2018.11.31/prop_2018_10_31/g"
 
+# find all files in a certain set of types (file extensions)
+find . -regex ".*\.\(yaml\|txt\)"
+
+# Find and replace.
 sed -i "s/python\/vat\/report/python\/report/g" Makefile
 
-find . -name "*.py" -print0 | xargs -0 sed -i "s/python\/vat\/build/python\/build/g"
-python/vat/build/
-
-find . -name "*.py" -print0 | xargs -0 sed -i "s/contractor/independiente/g"
-
+# Count lines added or deleted in the diff, ignore blank lines.
+# (This is the sum of additions and subtractions, not their difference.)
 git diff | egrep -v "^.$" | egrep "^\+" | wc
-  # count adds or deletes in the diff, ignore blank lines
 
-find . -name "*.py" -print0 | xargs -0 grep -i "todo"
+# Find in all files of one particular type.
+grep -i "todo" -r . --include="*.py"
+# Find (case-insensitive) in all files of many types
+find . -regex ".*\.\(py\|txt\|sh\|org\|mm\|md\|hs\)" -print0 | xargs -0 grep -i "todo"
 
-find . -name "*.py" -print0 | xargs -0 sed -ir "s/legends/purchase_file_legends/g"
+# Delete all files ending in "~" (the suffix on backups).
 find . -name "*~" -print0 | xargs -0 rm
-
-find . -type f -print0 | xargs -0 egrep -v "\.dta|\.csv|\.zip|Ig_|st2_sea_enc|\.git"
--- bash/make-all-models.sh
+++ bash/make-all-models.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
- for strategy in detail; do # detail | vat_holiday_3
-  for year in 2018; do # 2016 | 2018
+# Run some set of models, as determined by the values that
+# `strategy` and `year` iterate over. Edit those here;
+# provide the sample size at the command line.
+#
+# Usage:
+#   bash <this script> <sample size>
+# For instance, from within the docker image,
+# from the root of the project (which was mounted at /mnt):
+#   (base) root@127:/mnt# bash bash/make-all-models.sh 100
+
+for strategy in detail; do # options: detail | vat_holiday_3
+  for year in 2018; do # options: 2016 | 2018
     echo; echo "strategy:" $strategy $year; date
     make tests overview subsample=$1 \
                         regime_year=$year \
