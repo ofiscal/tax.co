583e42aa66ae2fe2ef2b1c478f817279a505d12b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-01-17 18:16:24 -0500

    document   all of docker/

---
+++ docker/commands.sh
@@ -0,0 +1,43 @@
+# PITFALL: This is not a script.
+# It is a collection of one-liners,
+# each to be run from the command line individually as needed.
+
+# A trick to quickly edit a Docker-locked file from the host system:
+# https://stackoverflow.com/a/26915343
+
+# Build a new image. Do this after making changes to the Dockerfile
+# or any of its dependencies.
+docker build -f Dockerfile -t           \
+  jeffreybbrown/tax.co:new .            \
+  | tee logs/"build-log.`date`.txt"
+
+# Change the name of the new image.
+docker tag jeffreybbrown/tax.co:new     \
+  jeffreybbrown/tax.co:<PUT A BETTER NAME HERE>
+
+# The rest of this script assumes the latest version of the image is
+# 2019-11-25.python-from-here
+
+docker tag jeffreybbrown/tax.co:new     \
+  jeffreybbrown/tax.co:2019-11-25.python-from-here
+
+# Upload the new image to DockerHub.
+docker push jeffreybbrown/tax.co:2019-11-25.python-from-here
+
+# Start a docker container based on the latest image.
+docker run --name tax -it		\
+  -v /home/jeff/tax.co/master:/mnt	\
+  -p 8888:8888 -d -h 127.0.0.1		\
+  jeffreybbrown/tax.co:2019-11-25.python-from-here
+
+# Start a docker container and run jupyter from within it.
+docker run --name tax -it		\
+  --entrypoint=/root/run-jupyter.sh	\
+  -v /home/jeff/tax.co:/mnt		\
+  -p 8888:8888 -d -h 127.0.0.1		\
+  jeffreybbrown/tax.co:2019-11-25.python-from-here
+
+# Start a shell within a running container.
+# (Once inside, go to the `/mnt` folder to do useful stuff,
+# like running `python` or the Makefile.)
+
