765ce47de2f794b039f4916e12f74e610c3cf44c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-02-04 00:39:57 -0500

    add csvtool to the Docker image

--- docker/commands.sh
+++ docker/commands.sh
@@ -13,29 +13,29 @@ docker build -f Dockerfile -t           \
 
 # Change the name of the new image.
 docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:<PUT A BETTER NAME HERE>
+  ofiscal/tax.co:2020-02-03.csvtool
 
 # The rest of this script assumes the latest version of the image is
-# 2020-01-05.django
+# 2020-02-03.csvtool
 
 docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:2020-01-05.django
+  ofiscal/tax.co:2020-02-03.csvtool
 
 # Upload the new image to DockerHub.
-docker push ofiscal/tax.co:2020-01-05.django
+docker push ofiscal/tax.co:2020-02-03.csvtool
 
 # Start a docker container based on the latest image.
 docker run --name tax -it		\
   -v /home/jeff/of/tax.co/master:/mnt	\
   -p 8888:8888 -d -h 127.0.0.1		\
-  ofiscal/tax.co:2020-01-05.django
+  ofiscal/tax.co:2020-02-03.csvtool
 
 # Start a docker container and run jupyter from within it.
 docker run --name tax -it		\
   --entrypoint=/root/run-jupyter.sh	\
   -v /home/jeff/of/tax.co:/mnt		\
   -p 8888:8888 -d -h 127.0.0.1		\
-  ofiscal/tax.co:2020-01-05.django
+  ofiscal/tax.co:2020-02-03.csvtool
 
 # Start a shell within a running container.
 # (Once inside, go to the `/mnt` folder to do useful stuff,
