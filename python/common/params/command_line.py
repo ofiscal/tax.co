82747cfc94ce2b1887e254bb27987357ff58bea4
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-02-17 15:43:50 -0500

    retire (delete) the vat-holiday code   That project was done before the end of 2019,   but some strategy-conditional logic had lingered until now.

--- python/common/params/command_line.py
+++ python/common/params/command_line.py
@@ -7,10 +7,7 @@ import python.common.terms as t
 
 
 strategy_names = [ # There used to be a lot of these.
-  t.detail,        # They disappeared in the branch "retire-hypotheticals".
-  t.vat_holiday_1,
-  t.vat_holiday_2,
-  t.vat_holiday_3
+  t.detail         # They disappeared in the branch "retire-hypotheticals".
 ]
 
 subsample = int( sys.argv[1] )
@@ -27,4 +24,3 @@ if not regime_year in [2016, 2018]:
 
 strategy_suffix = strategy
 strategy_year_suffix = strategy + "." + str(regime_year)
-
