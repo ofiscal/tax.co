89b03ee8972ff7d098f2ea6bb1b7db6887f263a6
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-03-17 20:56:13 -0500

    minor -- scripts, org

--- bash/make-all-models.sh
+++ bash/make-all-models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run some set of models, as determined by the values that
-# `strategy` and `year` iterate over. Edit those here;
+# `strategy` and `regime_year` iterate over. Edit those here;
 # provide the sample size at the command line.
 #
 # Usage:
@@ -10,11 +10,11 @@
 # from the root of the project (which was mounted at /mnt):
 #   (base) root@127:/mnt# bash bash/make-all-models.sh 100
 
-for strategy in detail; do # options: detail | vat_holiday_3
-  for year in 2018; do # options: 2016 | 2018
-    echo; echo "strategy:" $strategy $year; date
+for strategy in detail; do # options: detail
+  for regime_year in 2016 2018; do # options: 2016 | 2018
+    echo; echo "strategy:" $strategy $regime_year; date
     make tests overview subsample=$1 \
-                        regime_year=$year \
+                        regime_year=$regime_year \
                         strategy=$strategy
   done
 done
--- docker/commands.sh
+++ docker/commands.sh
@@ -10,8 +10,8 @@ docker exec -it tax bash
 
 # Build a new image. Do this after making changes to the Dockerfile
 # or any of its dependencies.
-docker build -f Dockerfile -t           \
-  ofiscal/tax.co:new .            \
+docker build -f Dockerfile -t \
+  ofiscal/tax.co:new .        \
   | tee logs/"build-log.`date`.txt"
 
 # Change the name of the new image.
@@ -28,16 +28,16 @@ docker rmi ofiscal/tax.co:new
 docker push ofiscal/tax.co:2020-03-12.csv-diff
 
 # Start a docker container based on the latest image.
-docker run --name tax -it		\
-  -v /home/jeff/of/tax.co/master:/mnt	\
-  -p 8888:8888 -d -h 127.0.0.1		\
+docker run --name tax -it             \
+  -v /home/jeff/of/tax.co/master:/mnt \
+  -p 8888:8888 -d -h 127.0.0.1        \
   ofiscal/tax.co:2020-03-12.csv-diff
 
 # Start a docker container and run jupyter from within it.
-docker run --name tax -it		\
-  --entrypoint=/root/run-jupyter.sh	\
-  -v /home/jeff/of/tax.co:/mnt		\
-  -p 8888:8888 -d -h 127.0.0.1		\
+docker run --name tax -it             \
+  --entrypoint=/root/run-jupyter.sh   \
+  -v /home/jeff/of/tax.co/master:/mnt \
+  -p 8888:8888 -d -h 127.0.0.1	      \
   ofiscal/tax.co:2020-03-12.csv-diff
 
 # Start a shell within a running container.
