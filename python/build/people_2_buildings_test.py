591def056e833d31bf9d0f0596df8820492a8e0f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-03-31 18:03:14 -0500

    2 bugfixes, both involving spelling

--- python/build/people_2_buildings_test.py
+++ python/build/people_2_buildings_test.py
@@ -1,13 +1,11 @@
 # The only thing to check is the increase in the set of columns.
 # (Could check length, but a left merge cannot change that.)
 
-
 if True:
   import sys
   import pandas as pd
   #
   import python.build.output_io as oio
-  import python.common.misc as c
   import python.common.common as c
 
 df1 = oio.readStage(c.subsample, 'people_1')
@@ -19,9 +17,9 @@ cs2 = set(df2.columns)
 
 assert set.union(cs1,new_cols) == cs2
 assert set.difference(cs2,cs1) == new_cols
-assert len( df1.columns ) == len( df2.columns ) + len( new_cols )
+assert len( df1.columns ) + len( new_cols ) == len( df2.columns )
   # This is useful since column names in Pandas need not be unique.
 
-oio.test_write( cm.subsample,
-                "people_2_buildings_test",
+oio.test_write( c.subsample,
+                "people_2_buildings",
                 "It worked." )
