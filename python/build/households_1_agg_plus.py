caeda05136629cabdff30806df9fc8c16338ef15
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-06-02 18:12:31 -0500

    bugfix   `households_1_agg_plus` was choking on the `edu` variable in the full sample.   It could not take the maximum on a data set for which the variable was everywhere null.   Solution was to handle that variable separately,   and when computing its maximum, drop households which have no edu value.

--- python/build/households_1_agg_plus.py
+++ python/build/households_1_agg_plus.py
@@ -15,13 +15,13 @@ if True:
   import python.build.households_1_agg_plus_defs as defs
 
 
-ppl = oio.readStage(
-  com.subsample,
-  "people_3_income_taxish." + com.strategy_year_suffix )
-
-ppl["edu"] = util.interpretCategorical(
-  ppl["edu"],
-  edu_key.values() )
+if True: # input
+  ppl = oio.readStage(
+    com.subsample,
+    "people_3_income_taxish." + com.strategy_year_suffix )
+  ppl["edu"] = util.interpretCategorical(
+    ppl["edu"],
+    edu_key.values() )
 
 if True: # compute five columns for top five member incomes
   # PITFALL: member-by-income is computed based only on labor income,
@@ -36,7 +36,7 @@ if True: # compute five columns for top five member incomes
     ppl["income"] * (ppl["member-by-income"] == 4) )
   ppl["income, rank 5"] = (
     ppl["income"] * (ppl["member-by-income"] == 5) )
-
+  #
   ppl["income, labor, rank 1"] = (
     ppl["income, labor"] * (ppl["member-by-income"] == 1) )
   ppl["income, labor, rank 2"] = (
@@ -69,21 +69,41 @@ if True: # aggregate from household members to households
   h_min["has-male"] = 1 - h_min["has-male"]
     # If female is 0 for anyone in a household, then min(female) = 0,
     # i.e. the household includes a male.
-  h_max = ppl.groupby(
-      ["household"]
-    ) [ defs.cols_to_min_or_max
-    ] . agg("max"
-    ) . rename( columns = {"age"            : "age-max",
-                           "literate"       : "has-lit",
-                           "student"        : "has-student",
-                           "edu"            : "edu-max",
-                           "female"         : "has-female",
-                           "race, indig"    : "has-indig",
-                           "race, git|rom"  : "has-git|rom",
-                           "race, raizal"   : "has-raizal",
-                           "race, palenq"   : "has-palenq",
-                           "race, whi|mest" : "has-whi|mest"
-    } )
+
+  if True: # Aggregating by max.
+      # This is complicated by the categorical `edu` variable.
+      # For some reason, if `edu` is missing for everyone in a household,
+      # Pandas throws an error instead of defining max-edu = np.nan.
+      # Therefore I handle the `edu` variable separately.
+    cols_without_edu = defs.cols_to_min_or_max.copy()
+      # PITFALL! Without the .copy() above, the next line
+      # would modify defs.cols_to_min_or_max.
+    cols_without_edu.remove("edu") # destructive side effect and no output
+    edu_max = ( ppl
+      [ ppl["edu"].notnull() ]
+      . groupby( ["household"] )
+      [[ "edu" ]]
+      . agg("max")
+      . rename( columns = { "edu" : "edu-max" } ) )
+    other_max = ppl.groupby(
+        ["household"]
+      ) [ cols_without_edu
+      ] . agg("max"
+      ) . rename( columns = {"age"            : "age-max",
+                             "literate"       : "has-lit",
+                             "student"        : "has-student",
+                             "female"         : "has-female",
+                             "race, indig"    : "has-indig",
+                             "race, git|rom"  : "has-git|rom",
+                             "race, raizal"   : "has-raizal",
+                             "race, palenq"   : "has-palenq",
+                             "race, whi|mest" : "has-whi|mest"
+      } )
+    h_max = pd.concat( [edu_max, other_max],
+                       axis = 1 )
+    del( edu_max, other_max )
+
+if True: # assemble the aggregates, then compute a few variables
   households = pd.concat( [h_first, h_sum, h_min, h_max]
                         , axis=1 )
 
