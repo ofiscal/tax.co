623adc9c17263851498e560457a8a543067a9ed1
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-06-26 18:58:20 -0500

    remake Dockerfile   to update pandas     (and other stuff, but the bugfixes in pandas might be important)a

--- docker/commands.sh
+++ docker/commands.sh
@@ -18,30 +18,30 @@ docker build -f Dockerfile -t \
 docker tag ofiscal/tax.co:new     \
   ofiscal/tax.co:latest
 docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:2020-06-16.pandas-1.03
+  ofiscal/tax.co:2020-06-26.pandas-1.05
 
 # The rest of this script assumes the latest version of the image is
 # 2020-02-03.csvtool
 docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:2020-06-16.pandas-1.03
+  ofiscal/tax.co:2020-06-26.pandas-1.05
 docker rmi ofiscal/tax.co:new
 
 # Upload the new image to DockerHub.
 docker push ofiscal/tax.co:latest
-docker push ofiscal/tax.co:2020-06-16.pandas-1.03
+docker push ofiscal/tax.co:2020-06-26.pandas-1.05
 
 # Start a docker container based on the latest image.
 docker run --name tax -it             \
   -v /home/jeff/of/tax.co/master:/mnt \
   -p 8888:8888 -d -h 127.0.0.1        \
-  ofiscal/tax.co:2020-06-16.pandas-1.03
+  ofiscal/tax.co:2020-06-26.pandas-1.05
 
 # Start a docker container and run jupyter from within it.
 docker run --name tax -it             \
   --entrypoint=/root/run-jupyter.sh   \
   -v /home/jeff/of/tax.co/master:/mnt \
   -p 8888:8888 -d -h 127.0.0.1	      \
-  ofiscal/tax.co:2020-06-16.pandas-1.03
+  ofiscal/tax.co:2020-06-26.pandas-1.05
 
 # Start a shell within a running container.
 # (Once inside, go to the `/mnt` folder to do useful stuff,
