cc349e9adf370242e7ed7faf49c55d54bf150522
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-08-13 19:40:10 -0500

    divide a module organize

--- proposal-2020-07-28/code/First.hs
+++ proposal-2020-07-28/code/First.hs
@@ -1,45 +1,14 @@
--- | This is Haskell code that generates Python code.
+-- | The first proposal.
+--
+-- This is Haskell code that generates Python code.
 -- For an example of how it is used, see the function
 -- `non_dividend_income_tax` in `python/regime/r2018.py`.
 
 {-# LANGUAGE NumericUnderscores #-}
+module First where
 
+import Lib
 
--- * Types
-
--- | A way to represent marginal tax rates.
-data MoneyBracket = MoneyBracket
-  { top :: Float
-  , rate :: Float } deriving (Show)
-
--- | A formula from money (usually income, sometimes wealth)
--- to total (not marginal) tax owed.
-data Formula = Formula
-  { fSubtract :: Float
-  , fRate :: Float
-  , fAdd :: Float
-  , fMax :: Float }
-
-instance Show Formula where
-  show f = "(x - " ++ show (fSubtract f) ++ " * muvt)*" ++
-           show (fRate f) ++ " + " ++ show (fAdd f)
-           ++ "*muvt if x < (" ++ show (fMax f) ++ "*muvt)"
-
-
--- * Functions
-
-go :: Formula -> [MoneyBracket] -> IO ()
-go initialFormula brackets =
-  mapM_ (putStrLn . show) $
-  scanl unMarginalize initialFormula brackets
-
-unMarginalize :: Formula -> MoneyBracket -> Formula
-unMarginalize prev bracket =
-  Formula { fSubtract = fMax prev
-          , fRate = rate bracket
-          , fAdd = fAdd prev +
-                   fRate prev * (fMax prev - fSubtract prev)
-          , fMax = top bracket }
 
 -- ** Data
 
