9745c91b463941f40917b7e1888720998b511ef3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-09-03 19:08:02 -0500

    Reorder r2019 similarly to r2018.

--- python/regime/r2019.py
+++ python/regime/r2019.py
@@ -17,63 +17,6 @@ income_tax_columns = [ "tax, income"
 
 gravable_pre = "cedula general gravable, sums before exemptions"
 
-def most_income_tax( income : float ) -> float:
-  # see test/income_tax_2018.hs for code that generates these formulas.
-  # Run this to test the accumulated totals
-  # (the numbers just after the + signs):
-  #   ns = [ 0.19 * (1700 - 1090),
-  #        0.28 * (4100 - 1700),
-  #        0.33 * (8670 - 4100),
-  #        0.35 * (18970 - 8670),
-  #        0.37 * (31000 - 18970) ]
-  #   for i in range(0,5):
-  #     print( sum( ns[0:i+1] ) )
-
-  x = income
-  return (
-    0                                           if x < (1090 *muvt)
-    else ( (x - 1090 *muvt)*0.19                if x < (1700 *muvt)
-    else ( (x - 1700 *muvt)*0.28 + 115.9  *muvt if x < (4100 *muvt)
-    else ( (x - 4100 *muvt)*0.33 + 787.9  *muvt if x < (8670 *muvt)
-    else ( (x - 8670 *muvt)*0.35 + 2296   *muvt if x < (18970*muvt)
-    else ( (x - 18970*muvt)*0.37 + 5901   *muvt if x < (31000*muvt)
-    else   (x - 31000*muvt)*0.39 + 10352.1*muvt
-    ) ) ) ) ) )
-
-def most_income_tax_proposed( income : float ) -> float:
-  x = income
-  return (
-    0                                           if x < ( 1090*muvt)
-    else ( (x -  1090 * muvt)*0.19              if x < ( 1700*muvt)
-    else ( (x -  1700 * muvt)*0.28 +   116*muvt if x < ( 4100*muvt)
-    else ( (x -  4100 * muvt)*0.33 +   788*muvt if x < ( 8670*muvt)
-    else ( (x -  8670 * muvt)*0.35 +  2296*muvt if x < (18970*muvt)
-    else ( (x - 18970 * muvt)*0.39 +  5901*muvt if x < (27595*muvt)
-    else ( (x - 27595 * muvt)*0.44 +  9265*muvt if x < (36000*muvt)
-    else ( (x - 36000 * muvt)*0.47 + 12963*muvt if x < (55000*muvt)
-    else ( (x - 55000 * muvt)*0.5  + 21893*muvt if x < (90000*muvt)
-    else   (x - 90000 * muvt)*0.55 + 39393*muvt
-    ) ) ) ) ) ) ) ) )
-
-def taxable( row: pd.Series ) -> float:
-  """
-  The first stage of "renta gravable laboral" is someone's income,
-  minus either 32.5% or 5040 UVTs, whichever is smaller.
-  If someone can claim no dependents, then their second stage renta gravable
-  is the same as the first.
-  If they can, and S1 is the value of the first stage,
-  then the second stage is equal to S1 minus 10% or 32 UVT,
-  whichever is smaller.
-  """
-  s1 = (
-    row               [gravable_pre]
-    - min( 0.325 * row[gravable_pre],
-           5040 * muvt ) )
-  s2 = ( s1 if not row["has dependent"]
-         else  s1 - min( 0.1 * s1,
-                         32 * muvt ) )
-  return s2
-
 def income_taxes( ppl : pd.DataFrame ) -> pd.DataFrame:
   """PITFALL: Destructive."""
   new_columns = pd.DataFrame()
@@ -155,3 +98,60 @@ def income_taxes( ppl : pd.DataFrame ) -> pd.DataFrame:
       sum( axis = "columns" ) )
 
   return pd.concat( [ppl, new_columns], axis = 1 )
+
+def most_income_tax( income : float ) -> float:
+  # see test/income_tax_2018.hs for code that generates these formulas.
+  # Run this to test the accumulated totals
+  # (the numbers just after the + signs):
+  #   ns = [ 0.19 * (1700 - 1090),
+  #        0.28 * (4100 - 1700),
+  #        0.33 * (8670 - 4100),
+  #        0.35 * (18970 - 8670),
+  #        0.37 * (31000 - 18970) ]
+  #   for i in range(0,5):
+  #     print( sum( ns[0:i+1] ) )
+
+  x = income
+  return (
+    0                                           if x < (1090 *muvt)
+    else ( (x - 1090 *muvt)*0.19                if x < (1700 *muvt)
+    else ( (x - 1700 *muvt)*0.28 + 115.9  *muvt if x < (4100 *muvt)
+    else ( (x - 4100 *muvt)*0.33 + 787.9  *muvt if x < (8670 *muvt)
+    else ( (x - 8670 *muvt)*0.35 + 2296   *muvt if x < (18970*muvt)
+    else ( (x - 18970*muvt)*0.37 + 5901   *muvt if x < (31000*muvt)
+    else   (x - 31000*muvt)*0.39 + 10352.1*muvt
+    ) ) ) ) ) )
+
+def most_income_tax_proposed( income : float ) -> float:
+  x = income
+  return (
+    0                                           if x < ( 1090*muvt)
+    else ( (x -  1090 * muvt)*0.19              if x < ( 1700*muvt)
+    else ( (x -  1700 * muvt)*0.28 +   116*muvt if x < ( 4100*muvt)
+    else ( (x -  4100 * muvt)*0.33 +   788*muvt if x < ( 8670*muvt)
+    else ( (x -  8670 * muvt)*0.35 +  2296*muvt if x < (18970*muvt)
+    else ( (x - 18970 * muvt)*0.39 +  5901*muvt if x < (27595*muvt)
+    else ( (x - 27595 * muvt)*0.44 +  9265*muvt if x < (36000*muvt)
+    else ( (x - 36000 * muvt)*0.47 + 12963*muvt if x < (55000*muvt)
+    else ( (x - 55000 * muvt)*0.5  + 21893*muvt if x < (90000*muvt)
+    else   (x - 90000 * muvt)*0.55 + 39393*muvt
+    ) ) ) ) ) ) ) ) )
+
+def taxable( row: pd.Series ) -> float:
+  """
+  The first stage of "renta gravable laboral" is someone's income,
+  minus either 32.5% or 5040 UVTs, whichever is smaller.
+  If someone can claim no dependents, then their second stage renta gravable
+  is the same as the first.
+  If they can, and S1 is the value of the first stage,
+  then the second stage is equal to S1 minus 10% or 32 UVT,
+  whichever is smaller.
+  """
+  s1 = (
+    row               [gravable_pre]
+    - min( 0.325 * row[gravable_pre],
+           5040 * muvt ) )
+  s2 = ( s1 if not row["has dependent"]
+         else  s1 - min( 0.1 * s1,
+                         32 * muvt ) )
+  return s2
