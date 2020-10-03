32e39a152e8f0437e1efdef2c1a2e897edcb4762
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-10-02 20:04:01 -0500

    Docker: upgrade Django

--- docker/commands.sh
+++ docker/commands.sh
@@ -5,7 +5,22 @@
 # A trick to quickly edit a Docker-locked file from the host system:
 # https://stackoverflow.com/a/26915343
 
-# start the image
+# Start a docker container based on the latest image.
+docker run --name tax -it             \
+  -v /home/jeff/of/tax.co/master:/mnt \
+  -p 8888:8888 -d -h 127.0.0.1        \
+  ofiscal/tax.co:2020-09-25.django-3.1.1
+
+# Start a docker container and run jupyter from within it.
+docker run --name tax -it             \
+  --entrypoint=/root/run-jupyter.sh   \
+  -v /home/jeff/of/tax.co/master:/mnt \
+  -p 8888:8888 -d -h 127.0.0.1	      \
+  ofiscal/tax.co:2020-09-25.django-3.1.1
+
+# Start a shell within a running container.
+# (Once inside, go to the `/mnt` folder to do useful stuff,
+# like running `python` or the Makefile.)
 docker exec -it tax bash
 
 # Build a new image. Do this after making changes to the Dockerfile
@@ -18,31 +33,8 @@ docker build -f Dockerfile -t \
 docker tag ofiscal/tax.co:new     \
   ofiscal/tax.co:latest
 docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:2020-06-26.pandas-1.05
+  ofiscal/tax.co:2020-09-25.django-3.1.1
 
-# The rest of this script assumes the latest version of the image is
-# 2020-02-03.csvtool
-docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:2020-06-26.pandas-1.05
-docker rmi ofiscal/tax.co:new
-
-# Upload the new image to DockerHub.
+# Upload to DockerHub.
 docker push ofiscal/tax.co:latest
-docker push ofiscal/tax.co:2020-06-26.pandas-1.05
-
-# Start a docker container based on the latest image.
-docker run --name tax -it             \
-  -v /home/jeff/of/tax.co/master:/mnt \
-  -p 8888:8888 -d -h 127.0.0.1        \
-  ofiscal/tax.co:2020-06-26.pandas-1.05
-
-# Start a docker container and run jupyter from within it.
-docker run --name tax -it             \
-  --entrypoint=/root/run-jupyter.sh   \
-  -v /home/jeff/of/tax.co/master:/mnt \
-  -p 8888:8888 -d -h 127.0.0.1	      \
-  ofiscal/tax.co:2020-06-26.pandas-1.05
-
-# Start a shell within a running container.
-# (Once inside, go to the `/mnt` folder to do useful stuff,
-# like running `python` or the Makefile.)
+docker push ofiscal/tax.co:2020-09-25.django-3.1.1
