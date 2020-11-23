93506fbaef6fc4103de5e3f87dbe79b067574acd
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-11-22 23:12:49 -0500

    Docker: install more stuff
      cron
      Python libraries yfinance, etc.

--- docker/commands.sh
+++ docker/commands.sh
@@ -9,14 +9,14 @@
 docker run --name tax -it             \
   -v /home/jeff/of/tax.co/master:/mnt \
   -p 8888:8888 -d -h 127.0.0.1        \
-  ofiscal/tax.co:2020-11-12.external-apache-conf
+  ofiscal/tax.co:2020-11-22.cron
 
 # Start a docker container and run jupyter from within it.
 docker run --name tax -it             \
   --entrypoint=/root/run-jupyter.sh   \
   -v /home/jeff/of/tax.co/master:/mnt \
   -p 8888:8888 -d -h 127.0.0.1	      \
-  ofiscal/tax.co:2020-11-12.external-apache-conf
+  ofiscal/tax.co:2020-11-22.cron
 
 # Start a shell within a running container.
 # (Once inside, go to the `/mnt` folder to do useful stuff,
@@ -34,9 +34,9 @@ docker build -f Dockerfile -t \
 docker tag ofiscal/tax.co:new     \
   ofiscal/tax.co:latest
 docker tag ofiscal/tax.co:new     \
-  ofiscal/tax.co:2020-11-12.external-apache-conf
+  ofiscal/tax.co:2020-11-22.cron
 docker rmi ofiscal/tax.co:new
 
 # Upload to DockerHub.
 docker push ofiscal/tax.co:latest
-docker push ofiscal/tax.co:2020-11-12.external-apache-conf
+docker push ofiscal/tax.co:2020-11-22.cron
