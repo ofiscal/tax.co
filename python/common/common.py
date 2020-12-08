9b8bd4f0f7dd54035d6e40a0d31d80e960c15a58
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-12-08 18:41:43 -0500

    common.py: read parameters from a JSON file

--- python/common/common.py
+++ python/common/common.py
@@ -2,20 +2,43 @@
 
 if True:
   from sys import argv
+  import json
   import pandas as pd
-  #
   import python.build.classes as cla
-  if len( argv) > 1: # if we are using the command line
-    import python.common.params.command_line as par
-  else:              # if we are in the interpreter
-    import python.common.params.repl         as par
+  import python.common.terms as terms
+  import sys
 
-if True:
-  subsample = par.subsample
-  strategy = par.strategy
-  regime_year = par.regime_year
-  strategy_suffix = par.strategy_suffix
-  strategy_year_suffix = par.strategy_year_suffix
+
+if len( argv) > 1: # If we are using the command line.
+  config_file = sys.argv[1]
+else:              # If we are in the interpreter.
+  config_file = "repl_params.json"
+
+if True: # For validation, mostly.
+         # (`valid_subsamples` is also looped over, in another program.)
+  valid_strategies = [ # There used to be a lot of these.
+    terms.detail       # They disappeared in the branch "retire-hypotheticals".
+  ]
+  valid_subsamples = [1,10,100,1000]
+  valid_regime_years = [2016, 2018, 2019]
+
+with open( config_file ) as f:
+  config_dict = json.load( f )
+
+subsample = int( config_dict["subsample"] )
+if not subsample in valid_subsamples:
+  raise ValueError( "invalid subsample reciprocal: " + str(subsample) )
+
+strategy = config_dict["strategy"]
+if not strategy in valid_strategies:
+  raise ValueError( "invalid strategy: " + strategy )
+
+regime_year = config_dict["regime_year"]
+if not regime_year in valid_regime_years:
+  raise ValueError( "invalid tax regime year: " + str(regime_year) )
+
+strategy_suffix = strategy
+strategy_year_suffix = strategy + "." + str(regime_year)
 
 def retrieve_file( file_struct, subsample ):
   return pd.read_csv(
