8b8c20b8f4dc5a15e1c799e1ceb609ad6ee0bcac
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-12-18 12:19:21 -0500

    MarginalTaxRatesToPython: works!
      It generates valid Python.

--- haskell/MarginalTaxRatesToPython.hs
+++ haskell/MarginalTaxRatesToPython.hs
@@ -2,7 +2,8 @@ module MarginalTaxRatesToPython where
 
 import Data.List (takeWhile)
 
--- * Types
+
+-- ** Types
 
 -- | A way to represent marginal tax rates.
 data MoneyBracket = MoneyBracket
@@ -23,13 +24,13 @@ instance Show Formula where
            ++ "*muvt if x < (" ++ show (fMax f) ++ "*muvt)"
 
 
--- | The first idiom.
--- The initial formula describes the first tax rate.
--- I'm not sure why I thought I needed to do that.
+-- ** Formatting the math.
+
+-- * The first, worse idiom.
 -- This was designed to be run from ghci; then I would copy, paste
 -- and format the resulting pythong code by hand.
---
--- PITFALL: This name sucks, but alas it is highly referred to,
+
+-- | PITFALL: This name sucks, but alas it is highly referred to,
 -- by programs that are not part of this cabal package.
 -- (If they were part of it I could just change the name and see in GHCI
 -- which references had broken.)
@@ -38,7 +39,28 @@ go initialFormula brackets =
   mapM_ (putStrLn . show) $
   scanl unMarginalize initialFormula brackets
 
--- | PITFALL: This hack only happens to work because the letter "i"
+
+-- * The later, better idiom.
+
+formatFormulas :: [Formula] -> [String]
+formatFormulas fs =
+  "def f(x):" : ( map ("  " ++) $
+                  wrapConditions $
+                  dropLastCondition $
+                  map show fs )
+
+wrapConditions :: [String] -> [String]
+wrapConditions (s:ss) = let
+  s' = "return ( " ++ s
+  ss' = map ("else ( " ++) ss
+  cap = replicate (length $ s:ss) ')'
+  in s' : ss' ++ [cap]
+
+-- | The last tax rate has no ceiling, so the "if x < _" condition
+-- does not apply. Moreover it *must* be dropped in order to be valid Python;
+-- otherwise it would be missing another "else" clause.
+--
+-- PITFALL: This hack only happens to work because the letter "i"
 -- only shows up in the word "if" in the formulas constructed by
 -- `bracketsToFormulas`.
 dropLastCondition :: [String] -> [String]
@@ -46,11 +68,15 @@ dropLastCondition ss =
   let last:earlier = reverse ss
   in reverse (takeWhile (/= 'i') last : earlier)
 
+
+-- ** The math, if it can be called that.
+
 bracketsToFormulas :: [MoneyBracket] -> [Formula]
 bracketsToFormulas bs = let
   f0 = initialFormula $ head bs
   in scanl unMarginalize f0 $ tail bs
 
+-- | The formula that applies to the taxpayers with the least money to tax.
 initialFormula :: MoneyBracket -> Formula
 initialFormula b = Formula 0 (rate b) 0 (top b)
 
--- haskell/MarginalTaxRatesToPython/Test.hs
+++ haskell/MarginalTaxRatesToPython/Test.hs
@@ -9,7 +9,28 @@ runTests = runTestTT tests
 
 tests :: Test
 tests = TestList
-  [ TestLabel "test_dropLastCondition" test_dropLastCondition ]
+  [ TestLabel "test_dropLastCondition" test_dropLastCondition
+  , TestLabel "test_wrapConditions" test_wrapConditions
+  , TestLabel "test_format" test_format
+  ]
+
+test_format :: Test
+test_format = TestCase $ do
+  let mb = MoneyBracket
+  assertBool "" $
+    formatFormulas (bracketsToFormulas [ mb 0 0
+                                       , mb 0.1 10
+                                       , mb 0.2 100 ] )
+    == [ "def f(x):"
+       , "  return ( (x - 0.0 * muvt)*0.0 + 0.0*muvt if x < (0.0*muvt)"
+       , "  else ( (x - 0.0 * muvt)*10.0 + 0.0*muvt if x < (0.1*muvt)"
+       , "  else ( (x - 0.1 * muvt)*100.0 + 1.0*muvt "
+       , "  )))" ]
+
+test_wrapConditions :: Test
+test_wrapConditions = TestCase $ do
+  assertBool "" $ wrapConditions ["a","b"]
+    == ["return ( a", "else ( b", "))" ]
 
 test_dropLastCondition :: Test
 test_dropLastCondition = TestCase $ do
