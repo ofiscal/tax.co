bf079add3d0b6785910918d5e77744e00846b441
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-01-20 18:36:34 -0500

    haskell: write a proper Main
      with IO, buildable to an executable,
      callable from the shell, with arguments.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import System.Environment (getArgs)
-import MarginalTaxRates
+import System.Exit as SE
+import MarginalTaxRates (csvToPython)
 
 
 main :: IO ()
-main = getArgs >>= mapM_ putStrLn
+main = do
+  (name_without_extension:_) <- getArgs
+  let inFile = name_without_extension ++ ".csv"
+      outFile = name_without_extension ++ ".py"
+  ess :: Either String [String] <-
+    csvToPython inFile
+  case ess of
+    Left s -> do putStrLn s
+                 SE.exitWith $ ExitFailure 1
+    Right ss -> writeFile outFile $ unlines ss
