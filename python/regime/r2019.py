5859e2dcd6af44d2312c0968b89d80dd0186246d
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-01-26 17:51:59 -0500

    r2019
      Delete old, non-csv-dynamic code.
      Add .csv files that generated the .py files.
      Improve some import names.

--- python/regime/r2019.py
+++ python/regime/r2019.py
@@ -7,17 +7,14 @@ if True: # imports
   if True: # csv-dynamic imports
     rates = com.marginal_rates_folder
     for lib in ["dividend",
-                "ocasional_low_tax",
-                "ocasional_high_tax",
+                "ocasional_low",
+                "ocasional_high",
                 "most",]:
       exec( "import "
             + ( path.join( rates, lib)
               . replace ( "/", "." ) )
-            + " as " + lib )
+            + " as rates_" + lib )
 
-
-# Set this to true to use the .py files in python/csv_dynamic/.
-csv_dynamic = True
 
 income_tax_columns = [ "tax, income"
                      , "tax, income, most"
@@ -47,26 +44,17 @@ def income_taxes( ppl : pd.DataFrame ) -> pd.DataFrame:
   new_columns["tax, income, most"] = (
     ( temp_columns["cedula general gravable"] +
       ppl["income, pension"] )
-    . apply( most . f
-             if csv_dynamic else
-             most_income_tax ) )
+    . apply( rates_most . f ) )
 
   new_columns["tax, income, dividend"] = (
     ppl["income, dividend"].apply(
-        dividend.f
-        if csv_dynamic else
-        lambda x:
-        0 if x < (300*muvt)
-        else (x - 300*muvt) * 0.1 ) )
+        rates_dividend.f ) )
 
   new_columns["tax, income, ganancia ocasional"] = (
-    ( ( ppl["income, ganancia ocasional, 10%-taxable"]
-        . apply ( ocasional_low_tax . f ) ) +
-      ( ppl["income, ganancia ocasional, 20%-taxable"]
-        . apply ( ocasional_high_tax . f ) ) )
-    if not csv_dynamic else
-    ( ppl["income, ganancia ocasional, 10%-taxable"] * 0.1 +
-      ppl["income, ganancia ocasional, 20%-taxable"] * 0.2 ) )
+    ( ppl["income, ganancia ocasional, 10%-taxable"]
+      . apply ( rates_ocasional_low . f ) ) +
+    ( ppl["income, ganancia ocasional, 20%-taxable"]
+      . apply ( rates_ocasional_high . f ) ) )
 
   # a.k.a. the "4 por mil" -- a 0.4% tax
   # levided on transactions involving someone's bank account.
@@ -84,18 +72,6 @@ def income_taxes( ppl : pd.DataFrame ) -> pd.DataFrame:
       sum( axis = "columns" ) )
 
   return pd.concat( [ppl, new_columns], axis = 1 )
-
-def most_income_tax( income : float ) -> float:
-  x = income
-  return (
-    0                                           if x < (1090 *muvt)
-    else ( (x - 1090 *muvt)*0.19                if x < (1700 *muvt)
-    else ( (x - 1700 *muvt)*0.28 + 115.9  *muvt if x < (4100 *muvt)
-    else ( (x - 4100 *muvt)*0.33 + 787.9  *muvt if x < (8670 *muvt)
-    else ( (x - 8670 *muvt)*0.35 + 2296   *muvt if x < (18970*muvt)
-    else ( (x - 18970*muvt)*0.37 + 5901   *muvt if x < (31000*muvt)
-    else   (x - 31000*muvt)*0.39 + 10352.1*muvt
-    ) ) ) ) ) )
 
 def taxable( row: pd.Series ) -> float:
   """
