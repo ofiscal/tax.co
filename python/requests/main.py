1d47570acb8fed27ccebe7e01c67d8def62f927e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-05-13 11:29:40 -0500

    minor - fix a typo in a command in a comment

--- python/requests/main.py
+++ python/requests/main.py
@@ -22,8 +22,8 @@
 # should be a path to a user configuration,
 # and the second should be an action to take.
 # For example,
-#   PYTHONPATH=/mnt/tax_co python3 python/requests/main.py users/1/config/config.json add-to-temp-queue
-#   PYTHONPATH=/mnt/tax:$PYTHONPATH_co python3 python/requests/main.py config/config.json try-to-advance-queue
+#   PYTHONPATH=/mnt/tax_co:$PYTHONPATH python3 python/requests/main.py users/1/config/config.json add-to-temp-queue
+#   PYTHONPATH=/mnt/tax_co:$PYTHONPATH python3 python/requests/main.py config/config.json try-to-advance-queue
 #       config.json isn't used for try-to-advance-queue,
 #       but still must be supplied, or common.py will be confused.
 # and for debugging:
