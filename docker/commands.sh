3de1e31d63a809789c3dc30d1399c05eaec7785c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-09-23 17:24:24 -0500

    Dockerfile: squash copy-paste bug

--- docker/commands.sh
+++ docker/commands.sh
@@ -5,7 +5,7 @@
 # A trick to quickly edit a Docker-locked file from the host system:
 # https://stackoverflow.com/a/26915343
 
-DOCKER_IMAGE_SUFFIX="2021-06-22.tex"
+DOCKER_IMAGE_SUFFIX="2021-09-23.inputrc"
 
 # Start a docker container based on the latest image.
 docker run --name tax -itd                   \
