8e7e3c4bed188fa002c4756810c06a5febeb7bcd
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2021-12-03 20:40:51 -0500

    Consumable-grouped VAT data
      Add a fourth stage to the code in that folder.
      It computes
        config/vat/grouped/consumable_groups.csv
      which is very similar to
        config/vat/fake_grouped/consumable_groups.csv
      except it has more data, the data is real,
      and it includes the "is prefix" column.

---
+++ python/build/grouped_vat/4.consumable_groups.py
@@ -0,0 +1,50 @@
+if True:
+  import pandas as pd
+  import numpy as np
+  import os
+  import re # regex
+  #
+  import python.build.grouped_vat.paths as paths
+
+
+if True: # input data
+  vat_coicop  = pd.read_csv ( paths.file_vat_coicop )
+  rate_groups = pd.read_csv ( "config/vat/rate_groups.csv" )
+
+if True: # COICOP prefix groups
+  prefixes = ( vat_coicop
+               . groupby ( "prefix" )
+               [["prefix vat"]]
+               . agg ( "first" )
+               . reset_index ()
+               . rename ( columns =
+                          {"prefix vat" : "rate"} )
+               .  merge( rate_groups,
+                         on = "rate" ) )
+  prefixes = prefixes.rename (
+    columns = {"prefix" : "consumable group"} )
+  prefixes["is prefix"] = True
+
+if True: # Other groups of consumables, e.g. "pink tax"
+  other_groups = (
+    pd.DataFrame (
+      { "consumable group" :
+        list ( set ( vat_coicop.columns )
+               - set ( [ "DESCRIPTION",
+                         "coicop",
+                         "Notes",
+                         "prefix",
+                         "vat",
+                         "vat, min",
+                         "vat, max",
+                         "prefix vat",
+                        ] ) ) } ) )
+  other_groups["rate group"] = 0
+  other_groups["is prefix"] = False
+
+consumable_groups = pd.concat (
+  [ prefixes     [[ "consumable group", "rate group", "is prefix" ]],
+    other_groups [[ "consumable group", "rate group", "is prefix" ]] ],
+  axis = "rows" )
+
+consumable_groups.to_csv ( paths.file_consumable_groups )
--- python/build/grouped_vat/run.sh
+++ python/build/grouped_vat/run.sh
@@ -3,6 +3,9 @@
 PYTHONPATH="/mnt/tax_co":"/opt/conda/lib/python3.8/site-packages"
 condaPython=/opt/conda/bin/python3
 
-PYTHONPATH=$PYTHONPATH $condaPython python/build/grouped_vat/1.coicop.py
-PYTHONPATH=$PYTHONPATH $condaPython python/build/grouped_vat/2.capitulo_c.py
-PYTHONPATH=$PYTHONPATH $condaPython python/build/grouped_vat/3.rename.py
+code_folder=python/build/grouped_vat/
+
+PYTHONPATH=$PYTHONPATH $condaPython $code_folder/1.coicop.py
+PYTHONPATH=$PYTHONPATH $condaPython $code_folder/2.capitulo_c.py
+PYTHONPATH=$PYTHONPATH $condaPython $code_folder/3.rename.py
+PYTHONPATH=$PYTHONPATH $condaPython $code_folder/4.consumable_groups.py
