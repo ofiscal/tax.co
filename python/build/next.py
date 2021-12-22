c885a4200c5c35e8b805cf592752868b4b39684f
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2021-12-21 20:35:10 -0500

    Toward using the VAT groups from tax.co.web
      Still missing:
        (1) Make this robust to partial group tables, by somehow
            merging it with a group table where everything has a VAT of zero.
        (2) Treat vat_cap_c the same as vat_coicop.
        (3) Ape anything remaining to ape from python/build/vat_rates.py
        (4) Save.

---
+++ python/build/next.py
@@ -0,0 +1,63 @@
+if True:
+  import os
+  import pandas                 as pd
+  #
+  import python.build.output_io as oio
+  import python.common.common   as c
+  import python.common.misc     as misc
+  import python.common.terms    as t
+
+
+consumables_by_coicop_prefix = (
+  pd.read_csv (
+    os.path.join ( "users",
+                   c . user,
+                   "config/vat/consumable_groups_by_coicop_prefix.csv" ) ) )
+
+consumables_other = (
+  pd.read_csv (
+    os.path.join ( "users",
+                   c . user,
+                   "config/vat/consumable_groups_other.csv" ) ) )
+
+vat_cap_c = (
+    misc . read_csv_or_xlsx (
+        os.path.join (
+          "config/vat/grouped/vat_by_capitulo_c" ),
+        encoding = "utf-8" )
+    . rename (
+        columns = { "CODE" : "25-broad-categs"
+                  , "DESCRIPTION" : "description" }
+    ) )
+
+vat_coicop = (
+    misc . read_csv_or_xlsx (
+        os.path.join (
+          "config/vat/grouped/vat_by_coicop" ),
+        encoding = "utf-8" ) )
+
+vat_coicop = vat_coicop.drop (
+  columns = ["prefix vat", "vat", "vat, min", "vat, max"] )
+vat_coicop = vat_coicop.merge (
+  consumables_by_coicop_prefix,
+  left_on = "prefix",
+  right_on = "group" )
+vat_coicop_orig = vat_coicop.copy()
+
+for g in consumables_other["group"]:
+  v = ( consumables_other.loc
+        [ consumables_other["group"] == g,
+          "vat" ]
+        . iloc[0] # Converts a single-valued series to a number.
+        )
+  vat_coicop[g] = vat_coicop[g] * v
+
+vat_coicop.to_csv( "multiplied.csv" )
+vat_coicop_orig.to_csv( "orig.csv" )
+
+### VIEW
+
+for i in vat_cap_c                    . columns : print(i)
+for i in vat_coicop                   . columns : print(i)
+for i in consumables_by_coicop_prefix . columns : print(i)
+for i in consumables_other            . columns : print(i)
