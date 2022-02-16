ff3f20beb71a06faf4da7d6f0d7e2efe93688ed3
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-02-16 09:33:53 -0500

    Move definitions of sets of variables
      to python/report/defs.py

--- python/report/defs.py
+++ python/report/defs.py
@@ -1,4 +1,169 @@
+if True:
+  from itertools import chain
+  import python.common.common    as com
+  if   com.regime_year == 2016:
+      import python.regime.r2016 as regime
+  elif com.regime_year == 2018:
+      import python.regime.r2018 as regime
+  else:
+      import python.regime.r2019 as regime
+
 def maybeFill(groupVar, val):
   if groupVar == "income-percentile":
     return val.zfill(2)
   else: return val
+
+groupVars = [ # Variables to group by
+  "one",
+  "female head",
+  "income-decile",
+  "income-percentile",
+  "income-percentile-in[90,97]",
+  "region-2",
+  ]
+
+#
+# Variables to summarize
+#
+
+commonVars = ( [
+  "income < min wage",
+  "pension, receiving",
+  "pension, contributing (if not pensioned)",
+  "pension, contributor(s) (if not pensioned) = split",
+  "pension, contributor(s) (if not pensioned) = self",
+  "pension, contributor(s) (if not pensioned) = employer",
+  "seguro de riesgos laborales",
+  "income",
+  "income, labor + cesantia",
+  "income, rental + interest",
+  "income, dividend",
+  "income, pension",
+  "income, govt",
+  "income, private",
+  "income, infrequent",
+  "female head",
+  "value, purchase",
+  "purchase value / income",
+  "vat paid",
+  "vat / income",
+  "vat / purchase value",
+  "value, tax, predial",
+  ] + regime.income_tax_columns + [
+    "tax, ss, pension",
+    "tax, ss, pension, employer",
+    "tax, ss, salud",
+    "tax, ss, salud, employer",
+    "tax, ss, solidaridad",
+    "tax, ss, parafiscales",
+    "tax, ss, cajas de compensacion",
+    "cesantias + primas",
+    "tax, income, gmf",
+    "tax, income, ganancia ocasional",
+  ] )
+
+householdSpecificVars = [
+  "(rank, labor income) = 1",
+  "(rank, labor income) = 2",
+  "(rank, labor income) = 3",
+  "(rank, labor income) = 4",
+  "(rank, labor income) = 5",
+  "members",
+  ]
+
+householdVars = commonVars + householdSpecificVars
+earnerVars    = commonVars
+
+
+#
+# Restricted sets of variables to summarize, for the simpler data frames
+#
+
+commonRestrictedVars = ( [
+  "income < min wage: mean",
+  "pension, receiving: mean",
+  "pension, receiving: min",
+  "pension, receiving: max",
+  "pension, contributing (if not pensioned): mean",
+  "pension, contributing (if not pensioned): min",
+  "pension, contributing (if not pensioned): max",
+  "pension, contributor(s) (if not pensioned) = split: mean",
+  "pension, contributor(s) (if not pensioned) = split: min",
+  "pension, contributor(s) (if not pensioned) = split: max",
+  "pension, contributor(s) (if not pensioned) = self: mean",
+  "pension, contributor(s) (if not pensioned) = self: min",
+  "pension, contributor(s) (if not pensioned) = self: max",
+  "pension, contributor(s) (if not pensioned) = employer: mean",
+  "pension, contributor(s) (if not pensioned) = employer: min",
+  "pension, contributor(s) (if not pensioned) = employer: max",
+  "seguro de riesgos laborales: mean",
+  "seguro de riesgos laborales: min",
+  "seguro de riesgos laborales: max",
+  "income: mean",
+  "income: min",
+  "income: max",
+  "income, labor + cesantia: mean",
+  "income, rental + interest: mean",
+  "income, dividend: mean",
+  "income, dividend: share",
+  "income, pension: mean",
+  "income, govt: mean",
+  "income, private: mean",
+  "income, infrequent: mean",
+  "female head: mean",
+  "value, purchase: median_unweighted",
+  "value, purchase: mean",
+  "vat paid: mean",
+  "purchase value / income: median_unweighted",
+  "purchase value / income: mean",
+  "vat / purchase value: median_unweighted",
+  "vat / purchase value: mean",
+  "vat / income: median_unweighted",
+  "vat / income: mean",
+  "value, tax, predial: median_unweighted",
+  "value, tax, predial: mean",
+  ]
+
+  # "chain.from_iterable" concatenates lists
+  + list( chain.from_iterable( [ [ c + ": median_unweighted"
+                                 , c + ": mean" ]
+                                 for c in regime.income_tax_columns ] ) )
+  + [
+    "tax, ss, pension: median_unweighted",
+    "tax, ss, pension: mean",
+    "tax, ss, pension, employer: median_unweighted",
+    "tax, ss, pension, employer: mean",
+    "tax, ss, salud: median_unweighted",
+    "tax, ss, salud: mean",
+    "tax, ss, salud, employer: median_unweighted",
+    "tax, ss, salud, employer: mean",
+    "tax, ss, solidaridad: median_unweighted",
+    "tax, ss, solidaridad: mean",
+    "tax, ss, parafiscales: median_unweighted",
+    "tax, ss, parafiscales: mean",
+    "tax, ss, cajas de compensacion: median_unweighted",
+    "tax, ss, cajas de compensacion: mean",
+    "cesantias + primas: median_unweighted",
+    "cesantias + primas: mean",
+    "tax, income, gmf: median_unweighted",
+    "tax, income, gmf: mean",
+    "tax, income, ganancia ocasional: median_unweighted",
+    "tax, income, ganancia ocasional: mean",
+  ] )
+
+householdSpecificRestrictedVars = [
+  "(rank, labor income) = 1: mean",
+  "(rank, labor income) = 1: mean_nonzero",
+  "(rank, labor income) = 2: mean",
+  "(rank, labor income) = 2: mean_nonzero",
+  "(rank, labor income) = 3: mean",
+  "(rank, labor income) = 3: mean_nonzero",
+  "(rank, labor income) = 4: mean",
+  "(rank, labor income) = 4: mean_nonzero",
+  "(rank, labor income) = 5: mean",
+  "(rank, labor income) = 5: mean_nonzero",
+  "members: mean",
+  ]
+
+householdRestrictedVars = commonRestrictedVars + householdSpecificRestrictedVars
+earnerRestrictedVars    = commonRestrictedVars
--- python/report/households.py
+++ python/report/households.py
@@ -3,21 +3,14 @@
 if True:
   import os
   import sys
-  import pandas as pd
-  from itertools import chain
+  import pandas                  as pd
   #
-  import python.build.output_io as oio
-  import python.common.common as com
-  import python.common.describe as desc
-  import python.common.misc as c
-  import python.draw.util as draw
-  import python.report.defs as defs
-  if   com.regime_year == 2016:
-      import python.regime.r2016 as regime
-  elif com.regime_year == 2018:
-      import python.regime.r2018 as regime
-  else:
-      import python.regime.r2019 as regime
+  import python.build.output_io  as oio
+  import python.common.common    as com
+  import python.common.describe  as desc
+  import python.common.misc      as c
+  import python.draw.util        as draw
+  import python.report.defs      as defs
 
 
 if True: # load data
@@ -43,161 +36,13 @@ if True: # Create a few columns missing in the input data.
     df["income < min wage"] = (
       df["income"] < c.min_wage )
 
-if True: # create a summary dataframe
-  commonVars = ( [
-      "income < min wage"
-    , "pension, receiving"
-    , "pension, contributing (if not pensioned)"
-    , "pension, contributor(s) (if not pensioned) = split"
-    , "pension, contributor(s) (if not pensioned) = self"
-    , "pension, contributor(s) (if not pensioned) = employer"
-    , "seguro de riesgos laborales"
-    , "income"
-    , "income, labor + cesantia"
-    , "income, rental + interest"
-    , "income, dividend"
-    , "income, pension"
-    , "income, govt"
-    , "income, private"
-    , "income, infrequent"
-    , "female head"
-    , "value, purchase"
-    , "purchase value / income"
-    , "vat paid"
-    , "vat / income"
-    , "vat / purchase value"
-    , "value, tax, predial" ]
-
-    + regime.income_tax_columns +
-
-    [ "tax, ss, pension"
-    , "tax, ss, pension, employer"
-    , "tax, ss, salud"
-    , "tax, ss, salud, employer"
-    , "tax, ss, solidaridad"
-    , "tax, ss, parafiscales"
-    , "tax, ss, cajas de compensacion"
-    , "cesantias + primas"
-    , "tax, income, gmf"
-    , "tax, income, ganancia ocasional"
-    ] )
-
-  householdSpecificVars = [
-    "(rank, labor income) = 1",
-    "(rank, labor income) = 2",
-    "(rank, labor income) = 3",
-    "(rank, labor income) = 4",
-    "(rank, labor income) = 5",
-    "members",
-    ]
-
-  householdVars = commonVars + householdSpecificVars
-  earnerVars    = commonVars
-
-  commonRestrictedVars = (
-    [ "income < min wage: mean"
-    , "pension, receiving: mean"
-    , "pension, receiving: min"
-    , "pension, receiving: max"
-    , "pension, contributing (if not pensioned): mean"
-    , "pension, contributing (if not pensioned): min"
-    , "pension, contributing (if not pensioned): max"
-    , "pension, contributor(s) (if not pensioned) = split: mean"
-    , "pension, contributor(s) (if not pensioned) = split: min"
-    , "pension, contributor(s) (if not pensioned) = split: max"
-    , "pension, contributor(s) (if not pensioned) = self: mean"
-    , "pension, contributor(s) (if not pensioned) = self: min"
-    , "pension, contributor(s) (if not pensioned) = self: max"
-    , "pension, contributor(s) (if not pensioned) = employer: mean"
-    , "pension, contributor(s) (if not pensioned) = employer: min"
-    , "pension, contributor(s) (if not pensioned) = employer: max"
-    , "seguro de riesgos laborales: mean"
-    , "seguro de riesgos laborales: min"
-    , "seguro de riesgos laborales: max"
-    , "income: mean"
-    , "income: min"
-    , "income: max"
-    , "income, labor + cesantia: mean"
-    , "income, rental + interest: mean"
-    , "income, dividend: mean"
-    , "income, dividend: share"
-    , "income, pension: mean"
-    , "income, govt: mean"
-    , "income, private: mean"
-    , "income, infrequent: mean"
-    , "female head: mean"
-    , "value, purchase: median_unweighted"
-    , "value, purchase: mean"
-    , "vat paid: mean"
-    , "purchase value / income: median_unweighted"
-    , "purchase value / income: mean"
-    , "vat / purchase value: median_unweighted"
-    , "vat / purchase value: mean"
-    , "vat / income: median_unweighted"
-    , "vat / income: mean"
-    , "value, tax, predial: median_unweighted"
-    , "value, tax, predial: mean" ]
-
-    # "chain.from_iterable" concatenates lists
-    + list( chain.from_iterable( [ [ c + ": median_unweighted"
-                                   , c + ": mean" ]
-                                   for c in regime.income_tax_columns ] ) )
-    +
-
-    [ "tax, ss, pension: median_unweighted"
-    , "tax, ss, pension: mean"
-    , "tax, ss, pension, employer: median_unweighted"
-    , "tax, ss, pension, employer: mean"
-    , "tax, ss, salud: median_unweighted"
-    , "tax, ss, salud: mean"
-    , "tax, ss, salud, employer: median_unweighted"
-    , "tax, ss, salud, employer: mean"
-    , "tax, ss, solidaridad: median_unweighted"
-    , "tax, ss, solidaridad: mean"
-    , "tax, ss, parafiscales: median_unweighted"
-    , "tax, ss, parafiscales: mean"
-    , "tax, ss, cajas de compensacion: median_unweighted"
-    , "tax, ss, cajas de compensacion: mean"
-    , "cesantias + primas: median_unweighted"
-    , "cesantias + primas: mean"
-    , "tax, income, gmf: median_unweighted"
-    , "tax, income, gmf: mean"
-    , "tax, income, ganancia ocasional: median_unweighted"
-    , "tax, income, ganancia ocasional: mean"
-    ] )
-
-  householdSpecificRestrictedVars = [
-    "(rank, labor income) = 1: mean",
-    "(rank, labor income) = 1: mean_nonzero",
-    "(rank, labor income) = 2: mean",
-    "(rank, labor income) = 2: mean_nonzero",
-    "(rank, labor income) = 3: mean",
-    "(rank, labor income) = 3: mean_nonzero",
-    "(rank, labor income) = 4: mean",
-    "(rank, labor income) = 4: mean_nonzero",
-    "(rank, labor income) = 5: mean",
-    "(rank, labor income) = 5: mean_nonzero",
-    "members: mean",
-    ]
-
-  householdRestrictedVars = commonRestrictedVars + householdSpecificRestrictedVars
-  earnerRestrictedVars    = commonRestrictedVars
-
-  groupVars = [ "one"
-              , "female head"
-              , "income-decile"
-              , "income-percentile"
-              , "income-percentile-in[90,97]"
-              , "region-2" ]
-
-  # PITFALL: Earlier, this looped over two data sets, households and people.
-  # Now its outermost loop could be flattened.
+if True: # Create a summary dataframe.
   summaryDict = {}
   for ( unit,         df,         vs,           ) in [
-      ( "households", households, householdVars ) ]:
+      ( "households", households, defs.householdVars ) ]:
 
     groupSummaries = []
-    for gv in groupVars:
+    for gv in defs.groupVars:
       varSummaries = []
       for v in vs:
         t = desc.tabulate_stats_by_group( df, gv, v, "weight" )
@@ -219,7 +64,7 @@ if True: # create a summary dataframe
                     ) . transpose()
 
 if True: # do the same thing to a subset of that data
-  df = df_tmi.loc [ householdRestrictedVars ]
+  df = df_tmi.loc [ defs.householdRestrictedVars ]
 
 df_tmi . reset_index ( inplace = True )
 df_tmi = df_tmi . rename ( columns = {"index" : "measure"} )
