493f381ab7d30fc35f60ccee9f1354e860b0999a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-03-31 17:22:15 -0500

    Demo: how to draw colored arrows over a bar chart.

--- python/draw/examples.py
+++ python/draw/examples.py
@@ -11,6 +11,35 @@ if True:
   matplotlib.use('Agg')
     # enable the previous line if calling from the (non-gui) shell
   import matplotlib.pyplot as plt
+  import matplotlib.patches as mpatches
+
+
+def arrows_on_bars ():
+  labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+  levels = [20, 35, 30, 35, 27]
+  changes = [10, 10, -10, -10, 1]
+  men_std = [2, 3, 4, 1, 2]
+  women_std = [3, 5, 2, 3, 3]
+  width = 0.35       # the width of the bars: can also be len(x) sequence
+
+  fig, ax = plt.subplots()
+
+  arrow = mpatches.FancyArrowPatch (
+    (0,0), # tail
+    (5,5), # head
+    color = "red",
+    mutation_scale = 30 ) # determines arrow width, among other things
+  ax.add_patch(arrow)
+
+  ax.bar(labels, levels, width, label='Men')
+  ax.set_ylabel('Scores')
+  ax.set_title('Words under the graph')
+  ax.legend()
+  if True: # alternatives
+    # plt.show()
+    plt.savefig("test-arrows.png")
+  plt.close()
+
 
 def a_bar_chart ():
   plt.bar ( x = [1,2,3],
