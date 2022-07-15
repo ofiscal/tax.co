52d088113e91ba93f15e474ad39bf59aa2e32a6c
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-07-15 17:07:30 -0500

    Write some tests
      They test the equivalence of the old (code-) and new (.csv-based)
      idioms for defining SS tax schedules.

--- python/build/ss/schedules_test.py
+++ python/build/ss/schedules_test.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 import python.build.ss.schedules as sss
+from   python.common.misc import min_wage
+from   python.common.util import near
 
+
+#####################################################
+##### Test pension_contractor_schedule_from_frame
+##### without reading a CSV file.
+#####################################################
 
 pension_contractor_frame = pd.DataFrame (
   { "min_threshold_in_min_wages" : [0    , 1   ],
@@ -14,10 +21,50 @@ pension_contractor_schedule_from_frame = sss.ss_tax_schedule_from_frame (
   df = pension_contractor_frame )
 
 for b in [0,1]: # tax bracket
-  for i in [0,2]: # list index
-    assert ( pension_contractor_schedule_from_frame             [0][i] ==
-             sss.ss_contrib_schedule_for_contractor ["pension"] [0][i] )
+  for i in [0,2]: assert ( near ( # list index
+      pension_contractor_schedule_from_frame             [0][i],
+      sss.ss_contrib_schedule_for_contractor ["pension"] [0][i] ) )
 
-  for w in range(25): # wage
-    assert ( pension_contractor_schedule_from_frame             [0][1] (w) ==
-             sss.ss_contrib_schedule_for_contractor ["pension"] [0][1] (w) )
+  for w in range(25): assert ( near ( # wage
+      pension_contractor_schedule_from_frame             [0][1] (w),
+      sss.ss_contrib_schedule_for_contractor ["pension"] [0][1] (w) ) )
+
+
+#########################################
+##### Test every SS tax-encoding CSV file
+#########################################
+
+for (new, old) in \
+    [ (sss.ss_contrib_schedule_for_contractor_new,
+       sss.ss_contrib_schedule_for_contractor),
+      (sss.ss_contrib_schedule_for_employee_new,
+       sss.ss_contrib_schedule_for_employee),
+     ]:
+  for t in [ "pension", "salud", "solidaridad"]: # tax
+    for b in range ( len ( old [t] ) ): # tax bracket
+      for i in [0,2]: # list index
+        assert ( near (
+          old [t] [b] [i],
+          new [t] [b] [i] ) )
+
+      for w in range(30): # wage
+        assert ( near (
+          old [t] [b] [1] (w * min_wage),
+          new [t] [b] [1] (w * min_wage) ) )
+
+for (new, old) in \
+    [ (sss.ss_contribs_by_employer_new,
+       sss.ss_contribs_by_employer), ]:
+  for t in [ "cajas de compensacion",
+             "cesantias + primas",
+             "parafiscales",
+             "pension",
+             "salud", ]:
+    for b in range ( len ( old [t] ) ): # tax bracket
+      for i in [0,2]: # list index
+        assert ( near ( old [t] [b] [i],
+                        new [t] [b] [i] ) )
+
+      for w in range(30): # wage
+        assert ( near ( old [t] [b] [1] (w * min_wage),
+                        new [t] [b] [1] (w * min_wage) ) )
