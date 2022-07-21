b3f8b709fe49da3b2b93af93ffddadcecb79a534
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-07-21 16:28:14 -0500

    Read the tabular-encoded SS tax schedules into dicts,
      which should (tests forthcoming) be equivalent to the old,
      non-tabular idiom defining similarly-shaped and -named dictionaries.

--- python/build/ss/schedules.py
+++ python/build/ss/schedules.py
@@ -13,8 +13,9 @@ such that SS schedules are represented as CSV files.
 """
 
 
+import os
 import pandas as pd
-from typing import Callable, Dict, List, Tuple
+from   typing import Callable, Dict, List, Tuple
 
 from python.common.misc import min_wage
 
@@ -48,6 +49,48 @@ def ss_tax_schedule_from_frame (
                  "lambda",
                  "average_tax_rate" ]]
            . values . tolist () )
+
+def ss_tax_schedule_from_csv (
+    basename : str,
+) -> List [ Tuple [ float,                       # minimum income threshold
+                    Callable [ [float], float ], # computes taxable base from wage
+                    float ]]:                    # average (not marginal!) tax rate
+  return ss_tax_schedule_from_frame (
+    pd.read_csv (
+      os.path.join ( "data/ss/",
+                     basename,
+                     ".csv") ) )
+
+ss_contrib_schedule_for_contractor_new : \
+  Dict [ str,
+         List [ Tuple [ float,                       # minimum income threshold
+                        Callable [ [float], float ], # computes taxable base from wage
+                        float ] ] ] = \              # average (not marginal!) tax rate
+  { "pension"     : ss_tax_schedule_from_csv ( "contractor_pension" ),
+    "salud"       : ss_tax_schedule_from_csv ( "contractor_salud" ),
+    "solidaridad" : ss_tax_schedule_from_csv ( "contractor_solidaridad" ), }
+
+ss_contrib_schedule_for_employee_new : \
+  Dict [ str,
+         List [ Tuple [ float,                       # minimum income threshold
+                        Callable [ [float], float ], # computes taxable base from wage
+                        float ] ] ] = \              # average (not marginal!) tax rate
+  { "pension"     : ss_tax_schedule_from_csv ( "employee_pension" ),
+    "salud"       : ss_tax_schedule_from_csv ( "employee_salud" ),
+    "solidaridad" : ss_tax_schedule_from_csv ( "employee_solidaridad" ), }
+
+# For employees, but not contractors,
+# some contributions are also made by the employer.
+ss_contribs_by_employer_new : \
+  Dict [ str,
+         List [ Tuple [ float,                       # minimum income threshold
+                        Callable [ [float], float ], # computes taxable base from wage
+                        float ] ] ] = \              # average (not marginal!) tax rate
+ { "pension"              : ss_tax_schedule_from_csv ( "employee_pension" ),
+  "cajas de compensacion" : ss_tax_schedule_from_csv ( "employer_cajas_de_compensacion" ),
+  "cesantias + primas"    : ss_tax_schedule_from_csv ( "employer_cesantias_y_primas" ),
+  "parafiscales"          : ss_tax_schedule_from_csv ( "employer_parafiscales" ),
+  "salud"                 : ss_tax_schedule_from_csv ( "employer_saluid" ), }
 
 
 ########################################
