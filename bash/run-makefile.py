98e05e958f6e8a8e59aca828eaeb678c62367d27
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-08-10 16:55:16 -0500

    run-makefile.py:
      Functionalize over users.
      Call that function for baseline and then for the user.

--- bash/run-makefile.py
+++ bash/run-makefile.py
@@ -37,31 +37,17 @@ PITFALL:
 from   datetime import datetime
 import os
 import subprocess
+from   typing import List
 
-import python.common.common as c
-import python.makefile_targets as makefile_targets
+import python.common.common     as c
+import python.common.terms      as terms
+import python.makefile_targets  as makefile_targets
 
 
 tax_co_root_path    = "/mnt/tax_co"
 
-logs_path = os.path.join(
-  tax_co_root_path, "users", c.user, "logs" )
-
-with open( os.path.join( logs_path, "make.txt"),
-           "a" ) as f:
-  f.write( "make.py starting at " + str( datetime.now() ) + "\n" )
-
-targets = (
-  # Makefile targets.
-  # For the full list of possible targets,
-  # see the Makefile, particularly the definition of .PHONY.
-  makefile_targets.targets
-  + ( [] # Since the baseline user is the one others are compared to,
-         # it makes no sense to build "compare" for tthat user.
-      if c.user_email == "baseline"
-      else ["compare"] ) )
-
-# TODO: This is duplicated in python/requests/main.py.
+# TODO: Some of this is duplicated,
+# in a less functional idiom, in python/requests/main.py.
 # Factor out both into a shared library.
 if True: # Refine the environment.
   my_env = os . environ . copy ()
@@ -76,27 +62,68 @@ if True: # Refine the environment.
       if "PYTHONPATH" in my_env . keys ()
       else env_additions )
 
-sp = subprocess.run (
-  ( [ "/usr/bin/make" ] +
-    targets +
-    [ "config_file" + "=" + c.config_file       ,
-      "subsample"   + "=" + str( c.subsample )  ,
-      "strategy"    + "=" + c.strategy          ,
-      "regime_year" + "=" + str( c.regime_year ),
-      "user"        + "=" + c.user,
-      "-k" # Keep going with other targets after any fails, if possible.
-     ] ),
-  cwd    = tax_co_root_path,
-  env    = my_env,
-  stdout = subprocess . PIPE,
-  stderr = subprocess . PIPE )
+def run_one_config (
+    config_file : str,
+    subsample   : int,
+    strategy    : str,
+    regime_year : int,
+    user_hash   : str,
+    targets     : List[str] ):
 
-for ( name, source ) in [ ("make.stdout.txt", sp.stdout),
-                          ("make.stderr.txt", sp.stderr) ]:
-  with open ( os.path.join ( logs_path, name ),
+  # logging
+  logs_path = os.path.join(
+    tax_co_root_path, "users", user_hash, "logs" )
+  with open( os.path.join( logs_path, "make.txt"),
              "a" ) as f:
-    f . write ( source . decode () )
+    f.write( "make.py starting at " + str( datetime.now() ) + "\n" )
 
-with open( os.path.join( logs_path, "make.txt"),
-           "a" ) as f:
-  f.write( "make.py ending at " + str( datetime.now() ) + "\n" )
+  # Run the Makefile.
+  # PITFALL: It uses the config file provided to subprocess.run,
+  # not necessarily the one used when this file was invoked.
+  sp = subprocess.run (
+    ( [ "/usr/bin/make" ] +
+      targets +
+      [ "config_file" + "=" + config_file,
+        "subsample"   + "=" + str( subsample ),
+        "strategy"    + "=" + strategy,
+        "regime_year" + "=" + str( regime_year ),
+        "user"        + "=" + user_hash,
+        # "-k" # Keep going with other targets after any fails, if possible.
+       ] ),
+    cwd    = tax_co_root_path,
+    env    = my_env,
+    stdout = subprocess . PIPE,
+    stderr = subprocess . PIPE )
+
+  # logging
+  for ( name, source ) in [ ("make.stdout.txt", sp.stdout),
+                            ("make.stderr.txt", sp.stderr) ]:
+    with open ( os.path.join ( logs_path, name ),
+               "a" ) as f:
+      f . write ( source . decode () )
+  with open( os.path.join( logs_path, "make.txt"),
+             "a" ) as f:
+    f.write( "make.py ending at " + str( datetime.now() ) + "\n" )
+
+run_one_config ( # First rebuild the baseline, if appropriate.
+                 # Use the full sample every time.
+  config_file = "users/symlinks/baseline/config/config.json",
+  subsample   = 1, # PITFALL: Unless baseline/config/config.json also has
+                 # subsample = 1, this will not build the right thing.
+  strategy    = terms.detail,
+  regime_year = 2019,
+  user_hash   = c.user_hash_from_email ( "baseline" ),
+  targets     = makefile_targets.targets )
+
+run_one_config ( # Then build the user data.
+  config_file = c.config_file,
+  subsample   = c.subsample,
+  strategy    = c.strategy,
+  regime_year = c.regime_year,
+  user_hash   = c.user,
+  targets     = (
+    makefile_targets.targets
+    + ( [] # Since the baseline user is the one others are compared to,
+        # it makes no sense to build "compare" for tthat user.
+        if c.user_email == "baseline"
+        else ["compare"] ) ) )
