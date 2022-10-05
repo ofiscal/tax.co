7727618d72501847e92386afb00214941cc0f684
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-10-05 12:33:35 -0500

    Improve the income tax deduction for dependents.
      Now it is subject to the same 5040 UVT maximum
      that applies to other deductions.
    
      The result is equal to the previous value, except
      for people with extremely high income and a dependent,
      for whom it makes a negligible difference,
      as can be verified with the following code:
    
      ```
        from   python.common.misc import muvt
        import pandas                  as pd
    
        def cgg_old ( row: pd.Series ) -> float:
          stage_1 = max ( ( row                 ["renta liquida"]
                            - min ( 0.325 * row ["renta liquida"],
                                    5040 * muvt ) ),
                          0 )
          return ( stage_1
                   if   not row["claims dependent (labor income tax)"]
                   else stage_1 - min ( 0.1 * stage_1,
                                        32 * muvt ) )
    
        def cgg_new ( row: pd.Series ) -> float:
          a1 = row ["renta liquida"] - 5040 * muvt
          x  = row ["renta liquida"] * (1 - 0.325)
          a2 = ( x
                 if   not row["claims dependent (labor income tax)"]
                 else x - min ( 0.1 * x,
                                32 * muvt ) )
          return max ( 0, a1, a2 )
    
        df = pd.DataFrame (
          { "renta liquida" :
            [1, 5, 1e1, 5e1, 1e2, 5e2, 1e3, 5e3, 1e4, 5e4, 1e5, 5e5, 1e6, 5e6, 1e7, 5e7, 1e8, 5e8, 1e9, 5e9],
            "claims dependent (labor income tax)":
            [1]*20 } )
    
        df["old"] = df.apply ( cgg_old, axis = 1 )
        df["new"] = df.apply ( cgg_new, axis = 1 )
        df["diff"] = df["new"] - df["old"]
        df
      ```

--- python/regime/cedula_general_gravable.py
+++ python/regime/cedula_general_gravable.py
@@ -23,22 +23,19 @@ def cedula_general_gravable ( row: pd.Series ) -> float:
 
 def cgg_detail ( row: pd.Series ) -> float:
   """
-  `stage_1` is someone's income,
-  minus either 32.5% or 5040 UVTs, whichever is smaller.
-  If someone cannot claim dependents,
-  then the second stage equals the first.
-  If they can, then the second stage is equal to
-  stage_1 minus 10% or 32 UVT, whichever is smaller.
-  The second stage is the function's return value.
+  You can't deduct more than 5040 UVTs.
+  You can deduct somewhere between 25% and 40%,
+  and we don't know what you'll arrive at, so we assume the average, 32.5%.
+  After that you can deduct an additional 10% of what's left,
+  or 32 UVTs, whichever is smaller.
   """
-  stage_1 = max ( ( row                 ["renta liquida"]
-                    - min ( 0.325 * row ["renta liquida"],
-                            5040 * muvt ) ),
-                  0 )
-  return ( stage_1
-           if   not row["claims dependent (labor income tax)"]
-           else stage_1 - min ( 0.1 * stage_1,
-                                32 * muvt ) )
+  a1 = row ["renta liquida"] - 5040 * muvt
+  x  = row ["renta liquida"] * (1 - 0.325)
+  a2 = ( x
+         if   not row["claims dependent (labor income tax)"]
+         else x - min ( 0.1 * x,
+                        32 * muvt ) )
+  return max ( 0, a1, a2 )
 
 def cgg_reduce_income_tax_deduction_to_1210_uvts ( row: pd.Series ) -> float:
   stage_1 = max ( ( row                 ["renta liquida"]
