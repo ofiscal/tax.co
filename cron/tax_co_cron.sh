49f92831564aa31e873ee9c7848813490f99ebcb
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-05-12 16:18:34 -0500

    remove a straggling in-code mention of conda
      I think that eliminates all of them from the code
      (albeit not the docs).

--- cron/tax_co_cron.sh
+++ cron/tax_co_cron.sh
@@ -4,7 +4,7 @@
 
 cd /mnt/tax_co
 export PYTHONPATH=/mnt/tax_co:$PYTHONPATH
-/opt/conda/bin/python3      \
+python3                     \
     python/requests/main.py \
     config/config.json      \
     try-to-advance-queue    \
