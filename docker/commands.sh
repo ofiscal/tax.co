608a1bd027f4b06889f55b86de50ba95c93d1102
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-07-31 18:08:18 -0500

    update Docker recipe

--- docker/commands.sh
+++ docker/commands.sh
@@ -5,16 +5,16 @@
 # A trick to quickly edit a Docker-locked file from the host system:
 # https://stackoverflow.com/a/26915343
 
-DOCKER_IMAGE_SUFFIX="2023-05-10.no-conda"
+DOCKER_IMAGE_SUFFIX="2023-07-26.update-all"
 
 # Start a docker container based on the latest image.
-docker run --name tax -itd                   \
+docker run --name tax.co -itd                \
   -v /home/jeff/of/tax.co/online:/mnt/tax_co \
   -p 8888:8888 -h 127.0.0.1                  \
   ofiscal/tax.co:latest
 
 # Start a docker container and run jupyter from within it.
-docker run --name tax -itd                   \
+docker run --name tax.co -itd                   \
   --entrypoint=/root/run-jupyter.sh          \
   -v /home/jeff/of/tax.co/master:/mnt/tax_co \
   -p 8888:8888 -h 127.0.0.1                  \
@@ -23,18 +23,18 @@ docker run --name tax -itd                   \
 # Start a shell within a running container.
 # (Once inside, go to the root of the project to do useful stuff,
 # like running `python` or `python/run-makefile.py`.)
-docker start tax
-docker exec -it tax bash # add -u to do it as root
+docker start tax.co
+docker exec -it tax.co bash # add -u to do it as root
 cd mnt/tax_co/
 
-docker stop tax && docker rm tax
+docker stop tax.co && docker rm tax.co
 
 # Build a new image.
 # Do this after making changes to the Dockerfile
 # or any of its dependencies.
 STARTING_AT=$(date)
 nice -n 19 docker build -f Dockerfile -t \
-  ofiscal/tax.co:new .        \
+  ofiscal/tax.co:new .                   \
     | tee logs/build-log."$STARTING_AT".txt
 echo $(date)
 tput bel # Make a noise to indicate termination.
