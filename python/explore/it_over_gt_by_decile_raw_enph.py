f741ecc887497c6af54ef0cdbdb06015f6015f6e
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-11-30 18:14:20 -0500

    exploring IT and GT: more
      In `python/explore/it_over_gt_by_decile_raw_enph.py`,
      functinoalize some code,
      fix a bug in the computation of the first weighted decile
      (it should depend on the income variable on which we sort),
      define two weighted first deciles
      (one based on IT, the other on IT per capita),
      compute a few more columns,
      improve some names,
      delete the commented-out tables at the end
      (use CSV files, not version-controlled, instead).

--- python/explore/it_over_gt_by_decile_raw_enph.py
+++ python/explore/it_over_gt_by_decile_raw_enph.py
@@ -39,9 +39,8 @@ if True: # Read and format the data
                      . str.replace ( ",", "." )
                      . astype (float) )
 
-
 # Compute number of members, merge into households,
-# and use to compute "IT/members" (income per capita)
+# and use to compute "IT per capita" (income per capita)
 if True:
   ppl_agg = ( # Find number of members in each household.
     ppl.groupby ( "household" )
@@ -54,100 +53,106 @@ if True:
                   how = "left",
                   on = "household" )
 
-  m["IT/members"] = ( # income per capita
+  m["IT per capita"] = (
     m["IT"] / m["members"] )
 
+if True: # Compute weighted deciles (janky!)
+  tenth_weight = m["weight"].sum() / 10
 
-if True: # Define and examine deciles, most of them unweighted.
-
-  decile_defining_var = "IT/members" # "IT" or "IT/members"
-
-  if True: # Computing the jankiest weighted lowest decile.
-    tenth_weight = m["weight"].sum() / 10
-    last_index = 7525 # I found this by hand.
+  if True: # First, define a weighted decile for "IT"
+    decile_defining_var = "IT"
+    m = m . sort_values ( decile_defining_var )
+    last_index = 7284 # I found this by hand, if sorting on IT
     assert m.iloc[:last_index + 1]["weight"].sum() >= tenth_weight
     assert m.iloc[:last_index]    ["weight"].sum() <  tenth_weight
-    weighted_lowest_decile = (
+
+    weighted_lowest_decile_sorted_on_it = (
       # The lowest decile is the first `n` poorest households,
       # such that the sum of their weights is one tenth of the total.
-      m
-      . sort_values ( decile_defining_var )
-      . iloc[:last_index] )
+      m . iloc[:last_index] . copy() )
 
+  if True: # Do it again for "IT per capita"
+    decile_defining_var = "IT per capita"
+    m = m . sort_values ( decile_defining_var )
+    last_index = 7224 # I found this by hand, if sorting on IT per capita
+    ratio = m.iloc[:last_index] ["weight"].sum() / tenth_weight
+    assert (ratio > 0.99) & (ratio < 1.01) # The last index is unstable, but near this.
+
+    weighted_lowest_decile_sorted_on_it_over_members = (
+      # The lowest decile is the first `n` poorest households,
+      # such that the sum of their weights is one tenth of the total.
+      m . iloc[:last_index] . copy() )
+
+  # Define unweighted deciles.
   m["decile, unweighted"] = pd.qcut (
-    # Define unweighted deciles.
     m[ decile_defining_var ],
     10,
     labels = False )
+
+def describe_deciles (
+    decile_defining_var : str, # "IT" or "IT per capita"
+    extra_weighted_decile : pd.DataFrame, # In addition to the unweighted deciles,
+                                          # must can include a weighted one.
+) -> pd.DataFrame:
 
   def describe_subset (
       # This will be used to describe deciles,
       # weighted or unweighted.
       subset_name : str,
-      df : pd.DataFrame ) -> pd.DataFrame:
+      df0 : pd.DataFrame ) -> pd.DataFrame:
+    df = df0.copy()
+    n_households = len ( df["household"] . drop_duplicates() )
+    assert len (df) == n_households # households are not repeated
+    weight_sum = df ["weight"] . sum ()
     return pd.DataFrame ( {
-      "Decile"              : [ subset_name ],
-      "Sum of IT"           : [   df["IT"] . sum() ],
-      "Sum of GT"           : [   df["GT"] . sum() ],
-      "Sum of IT, weighted" : [ ( df["IT"] * df["weight"] ) . sum() ],
-      "Sum of GT, weighted" : [ ( df["GT"] * df["weight"] ) . sum() ],
+      "Decile"              : [ subset_name                             ],
+      "n households"        : [ n_households                            ],
+      "Sum of weight"       : [   df["weight"]                  . sum() ],
+      "Sum of IT"           : [   df["IT"    ]                  . sum() ],
+      "Sum of GT"           : [   df["GT"    ]                  . sum() ],
+      "Sum of IT, weighted" : [ ( df["IT"    ] * df["weight"] ) . sum() ],
+      "Sum of GT, weighted" : [ ( df["GT"    ] * df["weight"] ) . sum() ],
     } )
 
-  accList = [] # a list in which to accumulate descriptions of deciles
-  accList.append (
+  summaryList = [] # a list in which to accumulate descriptions of deciles
+  summaryList.append (
     # Start by appending the weighted 0th decile.
-    describe_subset ( "weighted 0",
-                      weighted_lowest_decile ) )
+    describe_subset ( subset_name = "weighted 0",
+                      df0 = extra_weighted_decile ) )
   for i in range(10):
     # Then add all the unweighted deciles (including the 0th).
     decile = m[ m["decile, unweighted"] == i ]
-    accList.append (
+    summaryList.append (
       describe_subset ( str(i),
                         decile )
     )
 
-  acc : pd.DataFrame = (
+  summary : pd.DataFrame = (
     # Make a table out of those decile descriptions.
-    pd.concat ( accList )
+    pd.concat ( summaryList )
     . reset_index ( drop = True ) )
 
-  # Compute two more columns.
-  acc[ "Sum of GT / Sum of IT" ]   = ( acc [ "Sum of GT" ] /
-                                       acc [ "Sum of IT" ] )
-  acc[ "Wsum of GT / Wsum of IT" ] = ( acc [ "Sum of GT, weighted" ] /
-                                       acc [ "Sum of IT, weighted" ] )
+  # Compute a few more columns.
+  summary[ "Sum of GT / n households" ]  = ( summary [ "Sum of GT" ] /
+                                             summary [ "n households" ] )
+  summary[ "Sum of IT / n households" ]  = ( summary [ "Sum of IT" ] /
+                                             summary [ "n households" ] )
+  summary[ "Sum of GT / Sum of weight" ] = ( summary [ "Sum of GT" ] /
+                                             summary [ "Sum of weight" ] )
+  summary[ "Sum of IT / Sum of weight" ] = ( summary [ "Sum of IT" ] /
+                                             summary [ "Sum of weight" ] )
+  summary[ "Sum of GT / Sum of IT" ]     = ( summary [ "Sum of GT" ] /
+                                             summary [ "Sum of IT" ] )
+  summary[ "Wsum of GT / Wsum of IT" ]   = ( summary [ "Sum of GT, weighted" ] /
+                                             summary [ "Sum of IT, weighted" ] )
+  return summary
 
-
-acc # The result
-
-
-# Results, if decile_defining_var = "IT/members"
-#
-#      Decile     Sum of IT     Sum of GT  Sum of IT, weighted  Sum of GT, weighted  Sum of GT / Sum of IT  Wsum of GT / Wsum of IT
-#  weighted 0  2.902086e+09  6.745345e+09         5.937897e+11         1.216732e+12               2.324309                 2.049096
-#           0  3.768656e+09  7.978665e+09         7.587045e+11         1.441257e+12               2.117111                 1.899629
-#           1  7.941213e+09  9.984152e+09         1.455360e+12         1.775537e+12               1.257258                 1.219999
-#           2  9.542510e+09  1.038566e+10         1.574423e+12         1.737850e+12               1.088357                 1.103801
-#           3  1.193796e+10  1.191160e+10         1.864963e+12         1.931906e+12               0.997792                 1.035895
-#           4  1.400936e+10  1.293686e+10         2.155178e+12         2.043198e+12               0.923444                 0.948041
-#           5  1.664439e+10  1.443002e+10         2.513932e+12         2.273739e+12               0.866960                 0.904455
-#           6  1.877937e+10  1.549272e+10         3.017311e+12         2.593903e+12               0.824986                 0.859674
-#           7  2.292190e+10  1.742693e+10         3.368491e+12         2.662958e+12               0.760274                 0.790549
-#           8  3.005364e+10  2.131843e+10         4.347225e+12         3.311412e+12               0.709346                 0.761730
-#           9  6.338725e+10  3.573281e+10         1.124145e+13         7.625127e+12               0.563722                 0.678305
-
-
-# Results, if decile_defining_var = "IT"
-#
-#      Decile     Sum of IT     Sum of GT  Sum of IT, weighted  Sum of GT, weighted  Sum of GT / Sum of IT  Wsum of GT / Wsum of IT
-#  weighted 0  1.935811e+09  5.474161e+09         3.952538e+11         9.717309e+11               2.827839                 2.458499
-#           0  2.672727e+09  6.721673e+09         5.368087e+11         1.198133e+12               2.514912                 2.231955
-#           1  5.477734e+09  7.696810e+09         1.025721e+12         1.394381e+12               1.405108                 1.359416
-#           2  8.059080e+09  9.705304e+09         1.340730e+12         1.605673e+12               1.204269                 1.197611
-#           3  9.496003e+09  1.026278e+10         1.632191e+12         1.767233e+12               1.080747                 1.082736
-#           4  1.211099e+10  1.208627e+10         1.898405e+12         1.944570e+12               0.997959                 1.024317
-#           5  1.483465e+10  1.353978e+10         2.304079e+12         2.144411e+12               0.912713                 0.930702
-#           6  1.829672e+10  1.555565e+10         2.824643e+12         2.498798e+12               0.850188                 0.884642
-#           7  2.332340e+10  1.854458e+10         3.436916e+12         2.920132e+12               0.795106                 0.849637
-#           8  3.212062e+10  2.271873e+10         4.782152e+12         3.663942e+12               0.707294                 0.766170
-#           9  7.259432e+10  4.076627e+10         1.251539e+13         8.259613e+12               0.561563                 0.659957
+for ( decile_defining_var, extra_weighted_decile ) in [
+    ( "IT",            weighted_lowest_decile_sorted_on_it),
+    ( "IT per capita", weighted_lowest_decile_sorted_on_it_over_members) ]:
+  df = describe_deciles ( decile_defining_var = decile_defining_var,
+                          extra_weighted_decile = extra_weighted_decile )
+  print ( decile_defining_var )
+  print ( df )
+  df.to_csv ( os.path.join ( ".",
+                             decile_defining_var + ".csv" ) )
